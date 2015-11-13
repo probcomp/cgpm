fc917ab500e1cafadc42f3afd1487697828e6acb
F Saad <fsaad@mit.edu>
2015-11-12 22:45:02 -0500

    Rename MI tests.

--- gpmcc/tests/mi_tests/test_ring.py
+++ gpmcc/tests/mi_tests/test_ring.py
@@ -22,7 +22,6 @@ import math
 from gpmcc import state
 from gpmcc.utils import inference as iu
 
-
 W_list = [.9, .75, .5, .25, .1]
 n_data_sets = 3
 n_samples = 5
@@ -38,7 +37,6 @@ def _gen_ring(n,w):
         distance = random.uniform(1-w,1)
         X[i,0] = math.cos(angle)*distance
         X[i,1] = math.sin(angle)*distance
-
     return X+100
 
 for kernel in range(2):
@@ -53,23 +51,18 @@ for kernel in range(2):
             X = _gen_ring(N,w)
             for _ in range(n_samples):
                 S = state.State([X[:,0], X[:,1]], ['normal']*2,
-                    ct_kernel=kernel, distargs=[None]*2)
+                    distargs=[None]*2)
                 S.transition(N=200)
                 mi = iu.mutual_information(S, 0, 1)
                 # linfoot = iu.mutual_information_to_linfoot(MI)
                 MI[r,c] = mi
-
                 print("w: %1.2f, MI: %1.6f" % (w, mi))
                 print("%i of %i" % (i+1, len(W_list)*n_data_sets*n_samples*2))
-
                 del S
-
                 i += 1
                 r += 1
         c += 1
-
     w_labs = [str(w) for w in W_list]
-
     ax = pylab.subplot(1,2,kernel+1)
     pylab.boxplot(MI)
     pylab.ylim([0,1])
