372fbc5a2e289c2de603f10e3aefb9817540c013
F Saad <fsaad@mit.edu>
2015-11-28 13:23:50 -0500

    Enable recover test.

--- gpmcc/tests/inference/test_recover.py
+++ gpmcc/tests/inference/test_recover.py
@@ -13,62 +13,69 @@
 #   limitations under the License.
 
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
 
 from gpmcc.utils import test as tu
 from gpmcc.utils import sampling as su
-from gpmcc import state
+from gpmcc.engine import Engine
 
+shapes = ['x', 'sin', 'ring', 'dots']
 gen_function = {
-    'sin' : tu.gen_sine_wave,
-    'x' : tu.gen_x,
-    'ring' : tu.gen_ring,
-    'dots' : tu.gen_four_dots
+    'sin'   : tu.gen_sine_wave,
+    'x'     : tu.gen_x,
+    'ring'  : tu.gen_ring,
+    'dots'  : tu.gen_four_dots
 }
 
 cctypes = ['normal', 'normal']
 distargs = [None, None]
 
-shapes = ['x', 'sin', 'ring', 'dots']
 
-def run_test(argsin):
+def run_test(args):
     n_rows = args["num_rows"]
     n_iters = args["num_iters"]
     n_chains = args["num_chains"]
 
     n_per_chain = int(float(n_rows)/n_chains)
 
-    plt = 0
+    fig, axes = plt.subplots(nrows=2,ncols=4)
+    axes = axes.ravel()
+    k = 0
     for shape in shapes:
         print "Shape: %s" % shape
-        plt += 1
         T_o = gen_function[shape](n_rows)
         T_i = []
-        for chain in range(n_chains):
-            print "chain %i of %i" % (chain+1, n_chains)
-            S = state.State(T_o, cctypes, distargs=distargs)
-            S.transition(N=n_iters)
 
-            T_i.extend(su.simple_predictive_sample(S, n_rows, [0,1],
-                N=n_per_chain))
+        engine = Engine()
+        engine.initialize(T_o, cctypes, distargs, num_states=n_chains)
+        engine.transition(N=n_iters)
+
+        for chain in range(n_chains):
+            state = engine.get_state(chain)
+            print "chain %i of %i" % (chain+1, n_chains)
+            T_i.extend(su.simulate(state, -1, [0,1], N=n_per_chain))
 
         T_i = np.array(T_i)
-        ax = pylab.subplot(2,4,plt)
-        pylab.scatter( T_o[0], T_o[1], color='blue', edgecolor='none' )
-        pylab.ylabel("X")
-        pylab.ylabel("Y")
-        pylab.title("%s original" % shape)
-        pylab.subplot(2,4,plt+4)
-        pylab.scatter( T_i[:,0], T_i[:,1], color='red', edgecolor='none' )
-        pylab.ylabel("X")
-        pylab.ylabel("Y")
-        pylab.xlim(ax.get_xlim())
-        pylab.ylim(ax.get_ylim())
-        pylab.title("%s simulated" % shape)
+
+        ax = axes[k]
+        ax.scatter( T_o[0], T_o[1], color='blue', edgecolor='none' )
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_title("%s original" % shape)
+
+        ax = axes[k+4]
+        ax.scatter( T_i[:,0], T_i[:,1], color='red', edgecolor='none' )
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_xlim(ax.get_xlim())
+        ax.set_ylim(ax.get_ylim())
+        ax.set_title("%s simulated" % shape)
+
+        k += 1
 
     print "Done."
-    pylab.show()
+    plt.show()
 
 if __name__ == "__main__":
-    args = dict(num_rows=1000, num_iters=200, num_chains=1)
+    args = dict(num_rows=1000, num_iters=5000, num_chains=6)
     run_test(args)
