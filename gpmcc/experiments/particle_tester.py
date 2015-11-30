0ae47fb0776c7d0a2d7a572f423ca9d8c92e7b84
F Saad <fsaad@mit.edu>
2015-11-29 21:23:45 -0500

    Further organization.

--- gpmcc/experiments/particle_tester.py
+++ gpmcc/experiments/particle_tester.py
@@ -16,76 +16,73 @@ import multiprocessing
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.misc import logsumexp
 
 from gpmcc.utils import test as tu
 from gpmcc.utils import general as gu
-from particle_engine import ParticleEngine
 from particle_dim import ParticleDim
-
-
-def logmeanexp(values):
-    from scipy.misc import logsumexp
-    return logsumexp(values) - np.log(len(values))
 
 # Fix the dataset.
 np.random.seed(100)
-particles = 20
 
-# set up the data generation
-n_rows = 10
-view_weights = np.ones(1)
-cluster_weights = [ np.array([.5, .5]) ]
-cctypes = ['beta_uc']
-separation = [.8]
-distargs = [None]
+# Competing densities.
+dims = [
+    ParticleDim('normal'),
+    ParticleDim('exponential_uc'),
+    ParticleDim('lognormal'),
+    ParticleDim('beta_uc')
+    ]
 
-# T = np.random.normal(loc=1000, scale=10, size=100)
+def logmeanexp(values):
+    return logsumexp(values) - np.log(len(values))
 
 def _gibbs_transition(args):
     dim = args
     dim.gibbs_transition()
     return dim
 
+
+# Multiprocessor.
 pool = multiprocessing.Pool(multiprocessing.cpu_count())
-
-T, Zv, Zc, dims = tu.gen_data_table(n_rows, view_weights, cluster_weights,
-    cctypes, distargs, separation, return_dims=True)
-# dim.particle_learn(T[0])
-
-dims = [
-    ParticleDim('normal'),
-    ParticleDim('exponential_uc'),
-    ParticleDim('lognormal'),
-    ParticleDim('beta_uc')]
-
-def observe_datum(t):
-    for d in dims:
-        d.particle_learn([t])
-    return [d.weight for d in dims]
 
 def observe_data(t):
     global dims
+    global pool
     for d in dims:
         d.particle_learn([t])
-    weights = observe_datum(t)
+    weights = [d.weight for d in dims]
     print 'Observation %f: %f' % (dims[0].Nobs, t)
     print weights
     while True:
-        args = ((d) for d in dims)
-        dims = pool.map(_gibbs_transition, args)
+        dims = pool.map(_gibbs_transition, ((d) for d in dims))
         i = gu.log_pflip(weights)
         dims[i].gibbs_transition()
         ax.clear()
         dims[i].plot_dist(ax=ax, Y=np.linspace(0.01,0.99,200))
         ax.grid()
         plt.draw()
-        plt.pause(0.5)
+        plt.pause(1.5)
 
-plt.ion(); plt.show()
-_, ax = plt.subplots()
 def on_click(event):
     if event.button == 1:
         if event.inaxes is not None:
             observe_data(event.xdata)
 
+# Activate the plotter.
+plt.ion(); plt.show()
+_, ax = plt.subplots()
 plt.connect('button_press_event', on_click)
+
+# Data generation.
+n_rows = 100
+view_weights = np.ones(1)
+cluster_weights = [ np.array([.5, .5]) ]
+cctypes = ['beta_uc']
+separation = [.8]
+distargs = [None]
+T, Zv, Zc = tu.gen_data_table(n_rows, view_weights, cluster_weights,
+    cctypes, distargs, separation)
+
+# Test against synthetic data.
+# for t in T[0]:
+#     observe_data(t)
