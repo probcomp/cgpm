74e8ed2fa903998c9bcaa48c0796b2a6446ba9bb
F Saad <fsaad@mit.edu>
2015-12-25 17:52:12 +0300

    Small rewrite of View.
    See comment on line 134 for main change in this commit.

--- gpmcc/view.py
+++ gpmcc/view.py
@@ -64,57 +64,6 @@ class View(object):
             dim.reassign(X[:,dim.index], Zr)
             self.dims[dim.index] = dim
 
-    def transition_rows(self, target_rows=None):
-        """Reassign rows to clusters.
-        Keyword Arguments:
-        ... target_rows (list<int>): Rows to reassign. If None, transitions
-        every row.
-        """
-        if target_rows is None:
-            target_rows = [i for i in xrange(self.N)]
-
-        for rowid in target_rows:
-            # Get current assignment z_a.
-            z_a = self.Zr[rowid]
-            is_singleton = (self.Nk[z_a] == 1)
-
-            # Get CRP probabilities.
-            p_crp = list(self.Nk)
-            if is_singleton:
-                # If z_a is singleton do not consider a new singleton.
-                p_crp[z_a] = self.alpha
-            else:
-                # Decrement current cluster count.
-                p_crp[z_a] -= 1
-                # Append to the CRP an alpha for singleton.
-                p_crp.append(self.alpha)
-
-            # Log-normalize p_crp.
-            p_crp = np.log(np.array(p_crp))
-            p_crp = gu.log_normalize(p_crp)
-
-            # Calculate probability of rowid in each cluster k \in K.
-            p_cluster = []
-            for k in xrange(len(self.Nk)):
-                # If rowid is a singleton, compute new singleton logp by
-                # resampling the parameters.
-                if k == z_a and is_singleton:
-                    lp = self.row_predictive_logp(rowid, len(self.Nk)) + p_crp[k]
-                else:
-                    lp = self.row_predictive_logp(rowid, k) + p_crp[k]
-                p_cluster.append(lp)
-
-            # Propose singleton.
-            if not is_singleton:
-                lp = self.row_predictive_logp(rowid, len(self.Nk)) + p_crp[-1]
-                p_cluster.append(lp)
-
-            # Draw new assignment, z_b
-            z_b = gu.log_pflip(p_cluster)
-
-            # Migrate the row.
-            self.move_row_to_cluster(rowid, z_a, z_b)
-
     def transition(self, N):
         """Run all the transitions N times."""
         for _ in xrange(N):
@@ -150,25 +99,82 @@ class View(object):
         for _ in range(N):
             self.transition_rows(target_rows=target_rows)
 
-    def row_predictive_logp(self, rowid, cluster):
-        """Get the predictive log_p of rowid being in cluster. If cluster
+    def transition_rows(self, target_rows=None):
+        """Reassign rows to clusters.
+        Keyword Arguments:
+        ... target_rows (list<int>): Rows to reassign. If None, transitions
+        every row.
+        """
+        if target_rows is None:
+            target_rows = [i for i in xrange(self.N)]
+
+        for rowid in target_rows:
+            # Get current assignment z_a.
+            z_a = self.Zr[rowid]
+            is_singleton = (self.Nk[z_a] == 1)
+
+            # Get CRP probabilities.
+            p_crp = list(self.Nk)
+            if is_singleton:
+                # If z_a is singleton do not consider a new singleton.
+                p_crp[z_a] = self.alpha
+            else:
+                # Decrement current cluster count.
+                p_crp[z_a] -= 1
+                # Append to the CRP an alpha for singleton.
+                p_crp.append(self.alpha)
+
+            # Log-normalize p_crp.
+            p_crp = np.log(np.array(p_crp))
+            p_crp = gu.log_normalize(p_crp)
+
+            # Calculate probability of rowid in each cluster k \in K.
+            p_cluster = []
+            for k in xrange(len(self.Nk)):
+                # If k == z_a then predictive_logp will remove rowid's
+                # suffstats and reuse parameters.
+                lp = self.row_predictive_logp(rowid, k) + p_crp[k]
+                p_cluster.append(lp)
+
+            # Propose singleton.
+            if not is_singleton:
+                # Using len(self.Nk) will resample parameters.
+                lp = self.row_predictive_logp(rowid, len(self.Nk)) + \
+                    p_crp[-1]
+                p_cluster.append(lp)
+
+            # Draw new assignment, z_b
+            z_b = gu.log_pflip(p_cluster)
+
+            # Migrate the row.
+            self._move_row_to_cluster(rowid, z_a, z_b)
+
+    def row_predictive_logp(self, rowid, k):
+        """Get the predictive log_p of rowid being in cluster k. If k
         is existing (less than len(self.Nk)) then the predictive is taken.
-        If cluster is new (equal to len(self.Nk)) then new parameters
+        If k is new (equal to len(self.Nk)) then new parameters
         are sampled for the predictive."""
+        assert k <= len(self.Nk)
         logp = 0
         for dim in self.dims.values():
             x = self.X[rowid, dim.index]
-            # If rowid already in cluster, need to unincorporate first.
-            if self.Zr[rowid] == cluster:
-                assert cluster < len(self.Nk)
-                dim.unincorporate(x, cluster)
-                logp += dim.predictive_logp(x, cluster)
-                dim.incorporate(x, cluster)
+            # If rowid already in cluster k, need to unincorporate first.
+            if self.Zr[rowid] == k:
+                dim.unincorporate(x, k)
+                logp += dim.predictive_logp(x, k)
+                dim.incorporate(x, k)
             else:
-                logp += dim.predictive_logp(x, cluster)
+                logp += dim.predictive_logp(x, k)
         return logp
 
-    def move_row_to_cluster(self, rowid, move_from, move_to):
+    def insert_dim(self, dim):
+        dim.reassign(self.X[:, dim.index], self.Zr)
+        self.dims[dim.index] = dim
+
+    def remove_dim(self, dim_index):
+        del self.dims[dim_index]
+
+    def _move_row_to_cluster(self, rowid, move_from, move_to):
         """Move rowid from cluster move_from to move_to. If move_to
         is len(self.Nk) a new cluster will be created."""
         assert move_from < len(self.Nk) and move_to <= len(self.Nk)
@@ -182,16 +188,17 @@ class View(object):
             dim.unincorporate(self.X[rowid, dim.index], move_from)
             dim.incorporate(self.X[rowid, dim.index], move_to)
 
-        # Update partition and counts.
+        # Update partition and move_from counts.
         self.Zr[rowid] = move_to
         self.Nk[move_from] -= 1
 
-        # If move_to new cluster, update Nk.
+        # If move_to new cluster, extend Nk.
         if move_to == len(self.Nk):
             self.Nk.append(0)
             # Never create a singleton cluster from another singleton.
             assert self.Nk[move_from] != 0
 
+        # Update move_to counts.
         self.Nk[move_to] += 1
 
         # If move_from is now empty, delete and update cluster ids.
@@ -201,10 +208,3 @@ class View(object):
             for dim in self.dims.values():
                 dim.destroy_cluster(move_from)
             del self.Nk[move_from]
-
-    def insert_dim(self, dim):
-        dim.reassign(self.X[:, dim.index], self.Zr)
-        self.dims[dim.index] = dim
-
-    def remove_dim(self, dim_index):
-        del self.dims[dim_index]
