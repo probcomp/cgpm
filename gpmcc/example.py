112e50f2816e78a580d272cebbbce3a96232a88c
F Saad <fsaad@mit.edu>
2015-12-25 12:27:54 +0300

    Update example to give State a proper data matrix, not transpose.

--- gpmcc/example.py
+++ gpmcc/example.py
@@ -27,7 +27,7 @@ n_rows = 200
 view_weights = numpy.ones(1)
 cluster_weights = [ numpy.array([.33, .33, .34]) ]
 cctypes = ['beta_uc', 'normal','normal_uc','poisson','categorical(k=8)',
-    'vonmises','bernoulli', 'lognormal','exponential', 'exponential_uc',
+    'vonmises','bernoulli', 'lognormal', 'exponential',
     'geometric']
 separation = [.95] * len(cctypes)
 cctypes, distargs = cu.parse_distargs(cctypes)
@@ -35,5 +35,5 @@ cctypes, distargs = cu.parse_distargs(cctypes)
 T, Zv, Zc, dims = tu.gen_data_table(n_rows, view_weights, cluster_weights,
     cctypes, distargs, separation, return_dims=True)
 
-S = state.State(T, cctypes, distargs, seed=0)
+S = state.State(T.T, cctypes, distargs, seed=0)
 S.transition(N=10, do_plot=True)
