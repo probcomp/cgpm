3220a83390f52959cdbf373372855874019cfe36
F Saad <fsaad@mit.edu>
2016-01-15 20:15:30 -0500

    Remove bitrotted test_simulate for two-cluster.

--- gpmcc/tests/inference/test_simulate.py
+++ gpmcc/tests/inference/test_simulate.py
@@ -35,43 +35,6 @@ import gpmcc.utils.sampling as su
 import gpmcc.utils.general as gu
 from gpmcc.engine import Engine
 
-def test_predictive_draw(state, N=None):
-    if state.n_cols != 2:
-        print "State must have exactly 2 columns."
-        return
-
-    if N is None:
-        N = state.n_rows
-
-    view_1 = state.Zv[0]
-    view_2 = state.Zv[1]
-
-    if view_1 != view_2:
-        print "Columns not in same view."
-        return
-
-    log_crp = su.get_cluster_crps(state, 0)
-    K = len(log_crp)
-
-    X = np.zeros(N)
-    Y = np.zeros(N)
-
-    clusters_col_1 = su.create_cluster_set(state, 0)
-    clusters_col_2 = su.create_cluster_set(state, 1)
-
-    for i in xrange(N):
-        c = gu.log_pflip(log_crp)
-        x = clusters_col_1[c].predictive_draw()
-        y = clusters_col_2[c].predictive_draw()
-
-        X[i] = x
-        Y[i] = y
-
-    pylab.scatter(X,Y, color='red', label='inferred')
-    pylab.scatter(state.dims[0].X, state.dims[1].X, color='blue',
-        label='actual')
-    pylab.show()
-
 def test_simulate_indicator():
     # Entropy.
     np.random.seed(0)
