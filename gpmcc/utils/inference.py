4988f07b52b59f8d7b9c3d97f00bf095e7f85781
F Saad <fsaad@mit.edu>
2016-01-24 12:34:05 -0500

    Resurrect computation of mutual information.

--- gpmcc/utils/inference.py
+++ gpmcc/utils/inference.py
@@ -26,44 +26,38 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 # USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import math
+
+import numpy as np
+from scipy.misc import logsumexp
+
 import gpmcc.utils.sampling as su
 import gpmcc.utils.general as utils
 
-from scipy.misc import logsumexp
-
-import math
-import numpy
-
 def mutual_information_to_linfoot(MI):
-    return ( 1.0 - math.exp(-2.0 * MI)) ** 0.5
+    return (1.-math.exp(-2.*MI))**.5
 
-def mutual_information(state, col_1, col_2, N=1000):
-    view_1 = state.Zv[col_1]
-    view_2 = state.Zv[col_2]
+def mutual_information(state, col1, col2, N=1000):
+    if state.Zv[col1] != state.Zv[col2]:
+        return 0.
 
-    if view_1 != view_2:
-        print("mutual_information: not in same view: MI = 0.0")
-        return 0.0
-
-    log_crp = su.get_cluster_crps(state, view_1)
+    log_crp = su.compute_cluster_crp_logps(state, state.Zv[col1])
     K = len(log_crp)
-
-    clusters_col_1 = su.create_cluster_set(state, col_1)
-    clusters_col_2 = su.create_cluster_set(state, col_2)
+    clusters_col1 = su.create_clusters(state, col1)
+    clusters_col2 = su.create_clusters(state, col2)
 
     MI = 0
+    Px = np.zeros(K)
+    Py = np.zeros(K)
+    Pxy = np.zeros(K)
 
-    Px = numpy.zeros(K)
-    Py = numpy.zeros(K)
-    Pxy = numpy.zeros(K)
-
-    for i in range(N):
+    for _ in xrange(N):
         c = utils.log_pflip(log_crp)
-        x = clusters_col_1[c].predictive_draw()
-        y = clusters_col_2[c].predictive_draw()
+        x = clusters_col1[c].simulate()
+        y = clusters_col2[c].simulate()
         for k in range(K):
-            Px[k] = clusters_col_1[k].predictive_logp(x)
-            Py[k] = clusters_col_2[k].predictive_logp(y)
+            Px[k] = clusters_col1[k].predictive_logp(x)
+            Py[k] = clusters_col2[k].predictive_logp(y)
             Pxy[k] = Px[k] + Py[k] + log_crp[k]
             Px[k] += log_crp[k]
             Py[k] += log_crp[k]
@@ -73,8 +67,8 @@ def mutual_information(state, col_1, col_2, N=1000):
         MI += (PXY - PX - PY)
 
     MI /= float(N)
-    if MI < 0.0:
-        print("mutual_information: MI < 0 (%f)" % MI)
-        MI = 0.0
+    if MI < 0.:
+        print 'mutual_information: MI < 0 (%f)' % MI
+        MI = 0.
 
     return MI
