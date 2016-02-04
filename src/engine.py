bf0d81d3cfeafa6510c0d908cc686822cdbda171
F Saad <fsaad@mit.edu>
2016-02-04 18:13:51 -0500

    Implement multiproc functions using less duplication.
    - _initialize is special since it invokes a constructor.
    - _modify takes in metadata and args, and returns new metadata.
    - _evaluate takes in metadata and args, and returns value.

--- src/engine.py
+++ src/engine.py
@@ -34,51 +34,20 @@ import numpy as np
 
 from gpmcc.state import State
 
-# XXX Multiprocessing functions.
+# Multiprocessing functions.
 
 def _intialize((X, cctypes, distargs, seed)):
     state = State(X, cctypes, distargs=distargs, seed=seed)
     return state.to_metadata()
 
-def _transition((metadata, N, kernels, target_views, target_rows, target_cols)):
+def _modify((name, metadata, args)):
     state = State.from_metadata(metadata)
-    state.transition(N=N, kernels=kernels, target_views=target_views,
-        target_rows=target_rows, target_cols=target_cols)
+    getattr(state, name)(*args)
     return state.to_metadata()
 
-def _incorporate_dim((metadata, X, cctype, distargs, v)):
-    chain = State.from_metadata(metadata)
-    chain.incorporate_dim(X, cctype, distargs=distargs, v=v)
-    return chain.to_metadata()
-
-def _unincorporate_dim((metadata, col)):
+def _evaluate((name, metadata, args)):
     state = State.from_metadata(metadata)
-    state.unincorporate_dim(col)
-    return state.to_metadata()
-
-def _incorporate_rows((metadata, X, k)):
-    state = State.from_metadata(metadata)
-    state.incorporate_rows(X, k=k)
-    return state.to_metadata()
-
-def _unincorporate_rows((metadata, rowid)):
-    state = State.from_metadata(metadata)
-    state.unincorporate_rows(rowid)
-    return state.to_metadata()
-
-def _logpdf((metadata, rowid, query, evidence)):
-    state = State.from_metadata(metadata)
-    return state.logpdf(rowid, query, evidence=evidence)
-
-def _logpdf_marginal((metadata)):
-    state = State.from_metadata(metadata)
-    return state.logpdf_marginal()
-
-def _simulate((metadata, rowid, query, evidence, N)):
-    state = State.from_metadata(metadata)
-    return state.simulate(rowid, query, evidence=evidence, N=N)
-
-# XXX Multiprocessing functions.
+    return getattr(state, name)(*args)
 
 class Engine(object):
     """Multiprocessing engine for a stochastic ensemble of parallel States."""
@@ -102,52 +71,56 @@ class Engine(object):
         self.metadata = mapper(_intialize, args)
 
     def transition(self, N=1, kernels=None, target_views=None, target_rows=None,
-            target_cols=None, multithread=1):
+            target_cols=None, do_progress=True, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], N, kernels, target_views, target_rows,
-            target_cols) for i in xrange(self.num_states)]
-        self.metadata = mapper(_transition, args)
+        args = [('transition', self.metadata[i], (N, kernels, target_views,
+            target_rows, target_cols, do_progress)) for i in
+            xrange(self.num_states)]
+        self.metadata = mapper(_modify, args)
 
     def incorporate_dim(self, X, cctype, distargs=None, v=None, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], X, cctype, distargs, v) for i in
-            xrange(self.num_states)]
-        self.metadata = mapper(_incorporate_dim, args)
+        args = [('incorporate_dim', self.metadata[i],
+            (X, cctype, distargs, v)) for i in xrange(self.num_states)]
+        self.metadata = mapper(_modify, args)
 
     def unincorporate_dim(self, col, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], col) for i in xrange(self.num_states)]
-        self.metadata = mapper(_unincorporate_dim, args)
+        args = [('unincorporate_dim', self.metadata[i], (col,)) for i in
+            xrange(self.num_states)]
+        self.metadata = mapper(_modify, args)
 
     def incorporate_rows(self, X, k=None, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], X, k) for i in xrange(self.num_states)]
-        self.metadata = mapper(_incorporate_rows, args)
+        args = [('incorporate_rows', self.metadata[i], (X, k)) for i in
+            xrange(self.num_states)]
+        self.metadata = mapper(_modify, args)
 
     def unincorporate_rows(self, rowid, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], rowid) for i in
+        args = [('unincorporate_rows', self.metadata[i], (rowid,)) for i in
             xrange(self.num_states)]
-        self.metadata = mapper(_unincorporate_rows, args)
+        self.metadata = mapper(_modify, args)
 
     def logpdf(self, rowid, query, evidence=None, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], rowid, query, evidence) for i in
+        args = [('logpdf', self.metadata[i], (rowid, query, evidence)) for i in
             xrange(self.num_states)]
-        logpdfs = mapper(_logpdf, args)
+        logpdfs = mapper(_evaluate, args)
         return logpdfs
 
     def logpdf_marginal(self, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i]) for i in xrange(self.num_states)]
-        logpdf_marginals = mapper(_logpdf_marginal, args)
+        args = [('logpdf_marginal', self.metadata[i], ()) for i in
+            xrange(self.num_states)]
+        logpdf_marginals = mapper(_evaluate, args)
         return logpdf_marginals
 
     def simulate(self, rowid, query, evidence=None, N=1, multithread=1):
         _, mapper = self._get_mapper(multithread)
-        args = [(self.metadata[i], rowid, query, evidence, N) for i in
-            xrange(self.num_states)]
-        samples = mapper(_simulate, args)
+        args = [('simulate', self.metadata[i], (rowid, query, evidence, N)) for
+            i in xrange(self.num_states)]
+        samples = mapper(_evaluate, args)
         return samples
 
     def dependence_probability(self, col0, col1):
