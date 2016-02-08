2bc18e46e00336d9e27bf17d37b837ee89c83bcb
Gregory Marton <gremio@acm.org>
2016-02-08 15:18:59 -0500

    Addressed @riastradh's comments, and now checking for pytest only if actually about to use it.

--- check.sh
+++ check.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+set -Ceu
+
 : ${PYTHON:=python}
 
-lib_avail=`./pythenv.sh ${PYTHON} -m pytest --version 2>&1 | grep 'This is pytest'`
-if [ -z "$lib_avail" ]; then
-    printf >&2 'Unable to find pytest. Consider running python setup.py test.\n'
-    exit 1
-fi
-
 root=`cd -- "$(dirname -- "$0")" && pwd`
+
+function ensure_pytest() {
+    ok='This is pytest'
+    if ! ./pythenv.sh "$PYTHON" -m pytest --version 2>&1 | grep -q "$ok"; then
+        printf >&2 'ERROR: Unable to find pytest. Will not run tests.\n'
+        exit 1
+    fi
+}
 
 (
     set -Ceu
@@ -20,12 +24,14 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
         # By default, when running all tests, skip tests that have
         # been marked for continuous integration by using __ci_ in
         # their names.  (git grep __ci_ to find these.)
+        ensure_pytest
         ./pythenv.sh "$PYTHON" -m pytest -k "not __ci_" tests
     elif [ "docker" = "$1" ]; then
         docker build -t gpmcc .  # Runs check.sh inside the docker.
     else
         # If args are specified, run all tests, including continuous
         # integration tests, for the selected components.
+        ensure_pytest
         ./pythenv.sh "$PYTHON" -m pytest "$@"
     fi
 )
