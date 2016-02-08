2a15e2d06744f3602695ec7d68affd7a62f28b5c
Gregory Marton <gremio@acm.org>
2016-02-08 15:37:22 -0500

    As @riastradh suggests, let pytest fail on its own.

--- check.sh
+++ check.sh
@@ -6,14 +6,6 @@ set -Ceu
 
 root=`cd -- "$(dirname -- "$0")" && pwd`
 
-function ensure_pytest() {
-    ok='This is pytest'
-    if ! ./pythenv.sh "$PYTHON" -m pytest --version 2>&1 | grep -q "$ok"; then
-        printf >&2 'ERROR: Unable to find pytest. Will not run tests.\n'
-        exit 1
-    fi
-}
-
 (
     set -Ceu
     cd -- "${root}"
@@ -24,14 +16,12 @@ function ensure_pytest() {
         # By default, when running all tests, skip tests that have
         # been marked for continuous integration by using __ci_ in
         # their names.  (git grep __ci_ to find these.)
-        ensure_pytest
         ./pythenv.sh "$PYTHON" -m pytest -k "not __ci_" tests
     elif [ "docker" = "$1" ]; then
         docker build -t gpmcc .  # Runs check.sh inside the docker.
     else
         # If args are specified, run all tests, including continuous
         # integration tests, for the selected components.
-        ensure_pytest
         ./pythenv.sh "$PYTHON" -m pytest "$@"
     fi
 )
