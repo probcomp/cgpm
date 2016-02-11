8a6e5d5dc446eb992df219667c6de301b1eb1e4f
F Saad <fsaad@mit.edu>
2016-02-11 16:52:48 -0500

    Explicitly use distargs for engine init.

--- tests/test_binomial.py
+++ tests/test_binomial.py
@@ -41,7 +41,7 @@ class TestBinomial(unittest.TestCase):
         data = np.transpose(np.array([[0] * DATA_NUM_0 + [1] * DATA_NUM_1]))
         # Run a single chain for a few iterations.
         engine = gpmcc.engine.Engine(
-            data, ['categorical'], [{'k': 2}], initialize=True)
+            data, ['categorical'], distargs=[{'k': 2}], initialize=True)
         engine.transition(NUM_ITER)
         # Simulate from hypothetical row and compute the proportion of ones.
         xx = engine.simulate(-1, [0], N=NUM_SIM)[0]
