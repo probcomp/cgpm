71d79c0311dc1ab9df2bd4a2fdaa405282dca9c0
F Saad <fsaad@mit.edu>
2016-03-30 07:10:51 -0400

    Verify MI for dots is zero.

--- src/utils/xy_gpm/dots.py
+++ src/utils/xy_gpm/dots.py
@@ -15,35 +15,48 @@
 # limitations under the License.
 
 import numpy as np
+from scipy.stats import norm
+from scipy.misc import logsumexp
 
 from gpmcc.utils.xy_gpm import synthetic
 
 class DotsGpm(synthetic.SyntheticXyGpm):
     """(X,Y) ~ Four Dots"""
 
+    mx = [ -1, 1, -1, 1]
+    my = [ -1, -1, 1, 1]
+
     def simulate_xy(self, size=None):
         X = np.zeros((size,2))
-        mx = [ -1, 1, -1, 1]
-        my = [ -1, -1, 1, 1]
         for i in xrange(size):
             n = self.rng.randint(4)
-            x = self.rng.normal(loc=mx[n], scale=self.noise)
-            y = self.rng.normal(loc=my[n], scale=self.noise)
+            x = self.rng.normal(loc=self.mx[n], scale=self.noise)
+            y = self.rng.normal(loc=self.my[n], scale=self.noise)
             X[i,0] = x
             X[i,1] = y
         return X
 
     def logpdf_xy(self, x, y):
-        raise NotImplementedError
+        return logsumexp([np.log(.25)
+                + norm.logpdf(x, loc=mx, scale=self.noise)
+                + norm.logpdf(y, loc=my, scale=self.noise)
+            for (mx,my) in zip(self.mx, self.my)])
 
     def logpdf_x(self, x):
-        raise NotImplementedError
+        return logsumexp([np.log(.5) + norm.logpdf(x, loc=mx, scale=self.noise)
+            for mx in set(self.mx)])
 
     def logpdf_y(self, y):
-        raise NotImplementedError
+        return logsumexp([np.log(.5) + norm.logpdf(y, loc=my, scale=self.noise)
+            for my in set(self.my)])
 
     def logpdf_x_given_y(self, x, y):
         raise NotImplementedError
 
     def logpdf_y_given_x(self, y, x):
         raise NotImplementedError
+
+    def mutual_information(self):
+        samples = self.simulate_xy(size=1000)
+        return np.mean([self.logpdf_xy(x,y) - self.logpdf_x(x) -
+            self.logpdf_y(y) for [x,y] in samples])
