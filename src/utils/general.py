ded76e12af9deebf81865e28de7139e5948c2efc
F Saad <fsaad@mit.edu>
2016-04-06 18:51:55 -0400

    Use size=None by default which will do the right thing for existig and new code. Thanks @Riastradh.

--- src/utils/general.py
+++ src/utils/general.py
@@ -87,21 +87,21 @@ def logp_crp_gibbs(Nk, Z, i, alpha, m):
 
     return logp_tables + logp_aux
 
-def logp_crp_new(Nk, Z, alpha, m=1):
-    """Compute the CRP probabilities for a new customer i, with table counts Nk,
-    table assignments Z, and m auxiliary tables."""
+def logp_crp_fresh(Nk, Z, alpha, m=1):
+    """Compute the CRP probabilities for a fresh customer i=len(Z), with
+    table counts Nk, table assignments Z, and m auxiliary tables."""
     log_crp_numer = np.log(Nk + [alpha/m]*m)
     logp_crp_denom = log(len(Z) + alpha)
     return log_crp_numer - logp_crp_denom
 
-def log_pflip(logp, size=1, rng=None):
+def log_pflip(logp, size=None, rng=None):
     """Categorical draw from a vector logp of log probabilities."""
     if len(logp) == 1:
         return 0
     p = np.exp(log_normalize(logp))
     return pflip(p, size=size, rng=rng)
 
-def pflip(p, size=1, rng=None):
+def pflip(p, size=None, rng=None):
     """Categorical draw from a vector p of probabilities."""
     if len(p) == 1:
         return 0
@@ -110,8 +110,7 @@ def pflip(p, size=1, rng=None):
     p = normalize(p)
     if 10.**(-8.) < math.fabs(1.-sum(p)):
         warnings.warn('pflip probability vector sums to %f.' % sum(p))
-    flips = rng.choice(range(len(p)), size=size, p=p)
-    return flips[0] if size == 1 else flips
+    return rng.choice(range(len(p)), size=size, p=p)
 
 def log_linspace(a, b, n):
     """linspace from a to b with n entries over log scale (mor entries at
