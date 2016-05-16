8903065be377545e8b4f384e12071903ffc65d1a
F Saad <fsaad@mit.edu>
2016-05-16 14:13:57 -0400

    Partial working of random forest.

--- src/dists/forest.py
+++ src/dists/forest.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import OrderedDict
+
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
@@ -26,15 +28,23 @@ from gpmcc.dists.distribution import DistributionGpm
 class RandomForest(DistributionGpm):
     """RandomForest conditional distribution p(x|y) where x is categorical."""
 
-    def __init__(self, hypers=None, params=None, distargs=None, rng=None):
+    def __init__(self, outputs, inputs, hypers=None, params=None,
+            distargs=None, rng=None):
         self.rng = gu.gen_rng() if rng is None else rng
+        self.outputs = outputs
+        self.inputs = inputs
+        self.rng = gu.gen_rng() if rng is None else rng
+        assert len(self.outputs) == 1
+        assert len(self.inputs) >= 1
+        assert self.outputs[0] not in self.inputs
+        assert len(distargs['cctypes']) == len(self.inputs)
         # Number of output categories and input dimension.
         self.k = int(distargs['k'])
         self.p = len(distargs['cctypes'])
         # Sufficient statistics.
         self.N = 0
-        self.x = []
-        self.Y = []
+        self.x = OrderedDict()
+        self.Y = OrderedDict()
         self.counts = np.zeros(self.k)
         # Outlier and random forest parameters.
         if params is None: params = {}
@@ -43,37 +53,46 @@ class RandomForest(DistributionGpm):
         if self.regressor is None:
             self.regressor = RandomForestClassifier(random_state=self.rng)
 
-    def incorporate(self, x, y=None):
-        x, y = self.preprocess(x, y, self.get_distargs())
-        self.Y.append(y)
-        self.x.append(x)
-        self.counts[x] += 1
+    def incorporate(self, rowid, query, evidence):
+        assert rowid not in self.x
+        assert rowid not in self.Y
+        x, y = self.preprocess(query, evidence)
         self.N += 1
+        self.counts[x] += 1
+        self.x[rowid] = x
+        self.Y[rowid] = y
 
-    def unincorporate(self, x, y=None):
-        x, y = self.preprocess(x, y, self.get_distargs())
-        for i in xrange(len(self.Y)):
-            if np.allclose(self.Y[i], y) and self.x[i] == x:
-                del self.x[i], self.Y[i]
-                self.counts[x] -= 1
-                self.N -= 1
-                break
-        else:
-            raise ValueError('Observation %s not incorporated.' % str((x, y)))
+    def unincorporate(self, rowid):
+        del self.x[rowid]
+        del self.Y[rowid]
+        self.N -= 1
 
-    def logpdf(self, x, y=None):
-        try: x, y = RandomForest.preprocess(x, y, self.get_distargs())
-        except ValueError: return -float('inf')
+    def logpdf(self, rowid, query, evidence):
+        assert query.keys() == self.outputs
+        assert rowid not in self.x
+        try:
+            x, y = self.preprocess(query, evidence)
+        except ValueError:
+            return -float('inf')
         return RandomForest.calc_predictive_logp(
             x, y, self.regressor, self.counts, self.alpha)
 
+    def simulate(self, rowid, query, evidence):
+        assert query == self.outputs
+        if rowid in self.x:
+            return self.x[rowid]
+        logps = [self.logpdf(rowid, {query[0]:x}, evidence)
+            for x in xrange(self.k)]
+        return gu.log_pflip(logps, rng=self.rng)
+
     def logpdf_score(self):
         return RandomForest.calc_log_likelihood(
-            self.x, self.Y, self.regressor, self.counts, self.alpha)
+            self.x.values(), self.Y.values(), self.regressor,
+            self.counts, self.alpha)
 
-    def simulate(self, y=None):
-        logps = [self.logpdf(x, y=y) for x in xrange(self.k)]
-        return gu.log_pflip(logps, rng=self.rng)
+    ##################
+    # NON-GPM METHOD #
+    ##################
 
     def transition_params(self):
         # Transition noise parameter.
@@ -84,7 +103,7 @@ class RandomForest(DistributionGpm):
         self.alpha = alphas[index]
         # Transition forest.
         if len(self.Y) > 0:
-            self.regressor.fit(self.Y, self.x)
+            self.regressor.fit(self.Y.values(), self.x.values())
 
     def set_hypers(self, hypers):
         return
@@ -131,6 +150,19 @@ class RandomForest(DistributionGpm):
     # HELPER METHODS #
     ##################
 
+    def preprocess(self, query, evidence):
+        x = query[self.outputs[0]]
+        y = [evidence[c] for c in sorted(evidence)]
+        distargs = self.get_distargs()
+        p, k = distargs['p'], distargs['k']
+        if len(y) != p:
+            raise TypeError(
+                'RandomForest requires input length {}: {}'.format(p, y))
+        if not (x % 1 == 0 and 0 <= x < distargs['k']):
+            raise ValueError(
+                'RandomForest requires output in [0..{}): {}'.format(k, x))
+        return int(x), y
+
     @staticmethod
     def validate(x, K):
         return int(x) == float(x) and 0 <= x < K
@@ -153,14 +185,3 @@ class RandomForest(DistributionGpm):
             return logsumexp([
                 np.log(alpha) + logp_uniform,
                 np.log(1-alpha) + logp_rf])
-
-    @staticmethod
-    def preprocess(x, y, distargs=None):
-        p, k = distargs['p'], distargs['k']
-        if len(y) != p:
-            raise TypeError(
-                'RandomForest requires input length {}: {}'.format(p, y))
-        if (int(x) != float(x)) or not (0 <= x < distargs['k']):
-            raise ValueError(
-                'RandomForest requires output in [0..{}): {}'.format(k, x))
-        return int(x), y
