080f3afba95da6214de985fb413a24c78984bca3
F Saad <fsaad@mit.edu>
2016-05-24 17:25:41 -0400

    Implement incorporate/unincorporate dim correctly.

--- tests/test_incorporate_dim.py
+++ tests/test_incorporate_dim.py
@@ -57,14 +57,40 @@ def test_incorporate():
     assert state.Zv[10] == 0
     state.transition(N=1)
 
+    # Some crash testing queries.
+    state.logpdf(-1, [(10,1)], evidence=[(0,2), (1,1)])
+    state.simulate(-1, [10], evidence=[(0,2)])
+
     # Incorporating with a duplicated output should raise.
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         state.incorporate_dim(
             T[:,2], outputs=[10], cctype=CCTYPES[2], distargs=DISTARGS[2], v=0)
-        assert state.Zv[10] == 0
-        state.transition(N=1)
 
-    # Incorporate a new dim into a newly created singleton view.
+    # Multivariate incorporate should raise.
+    with pytest.raises(ValueError):
+        state.incorporate_dim(
+            T[:,2], outputs=[10, 2], cctype=CCTYPES[2],
+            distargs=DISTARGS[2], v=0)
+
+    # Missing output should raise.
+    with pytest.raises(ValueError):
+        state.incorporate_dim(
+            T[:,2], outputs=[], cctype=CCTYPES[2],
+            distargs=DISTARGS[2], v=0)
+
+    # Wrong number of rows should raise.
+    with pytest.raises(ValueError):
+        state.incorporate_dim(
+            T[:,2][:-1], outputs=[11], cctype=CCTYPES[2],
+            distargs=DISTARGS[2], v=0)
+
+    # Inputs should raise.
+    with pytest.raises(ValueError):
+        state.incorporate_dim(
+            T[:,2], outputs=[11], inputs=[2], cctype=CCTYPES[2],
+            distargs=DISTARGS[2], v=0)
+
+    # Incorporate dim into a newly created singleton view.
     state.incorporate_dim(
         T[:,3], outputs=[3], cctype=CCTYPES[3],
         distargs=DISTARGS[3], v=state.n_views())
@@ -91,23 +117,33 @@ def test_incorporate():
     state.incorporate_dim(
         T[:,5], outputs=[5], cctype=CCTYPES[5], distargs=DISTARGS[5],
         v=state.n_views())
-    previous = len(state.views)
+    previous = state.n_views()
     state.unincorporate_dim(5)
-    assert len(state.views) == previous-1
+    assert state.n_views() == previous-1
+    state.transition(N=1)
 
-    # # Reincorporate dim into a singleton view.
-    # state.incorporate_dim(T[:,5], CCTYPES[4], DISTARGS[4], v=len(state.views))
+    # Reincorporate dim into a singleton view.
+    state.incorporate_dim(T[:,5], outputs=[5], cctype=CCTYPES[5],
+        distargs=DISTARGS[5], v=len(state.views))
+    state.transition(N=1)
 
-    # # Incorporate the rest of the dims in the default way.
-    # for i in xrange(6, len(CCTYPES)):
-    #     state.incorporate_dim(T[:,i], CCTYPES[i], DISTARGS[i])
-    # assert state.n_cols() == T.shape[1]
+    # Incorporate the rest of the dims in the default way.
+    for i in xrange(6, len(CCTYPES)):
+        state.incorporate_dim(
+            T[:,i], outputs=[max(state.outputs)+1],
+            cctype=CCTYPES[i], distargs=DISTARGS[i])
+    state.transition(N=1)
 
-    # # Unincorporate all the dims, except the last one.
-    # for i in xrange(state.n_cols()-1, 0, -1):
-    #     state.unincorporate_dim(i)
-    # assert state.n_cols() == 1
+    # Unincorporating non-existent dim should raise.
+    with pytest.raises(ValueError):
+        state.unincorporate_dim(9999)
 
-    # # Unincorporating last dim should raise.
-    # with pytest.raises(ValueError):
-    #     state.unincorporate_dim(0)
+    # Unincorporate all the dims, except the last one.
+    for o in state.outputs[:-1]:
+        state.unincorporate_dim(o)
+    assert state.n_cols() == 1
+    state.transition(N=1)
+
+    # Unincorporating last dim should raise.
+    with pytest.raises(ValueError):
+        state.unincorporate_dim(state.outputs[0])
