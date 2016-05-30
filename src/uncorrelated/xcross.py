d380a28b74d8558700adc42df035048ffb9ee40b
F Saad <fsaad@mit.edu>
2016-05-30 15:43:44 -0400

    Convert XCross to CGPM.

--- src/uncorrelated/xcross.py
+++ src/uncorrelated/xcross.py
@@ -20,23 +20,59 @@ from scipy.misc import logsumexp
 from scipy.stats import multivariate_normal
 from scipy.stats import norm
 
-from gpmcc.uncorrelated import synthetic
+from gpmcc.gpm import Gpm
+from gpmcc.utils.general import gen_rng
 
 
-class XCrossGpm(synthetic.SyntheticXyGpm):
+class XCross(Gpm):
     """Y = (+/- w.p .5) X + N(0,noise)."""
 
-    def simulate_xy(self, size=None):
-        X = np.zeros((size,2))
-        for i in xrange(size):
-            if self.rng.rand() < .5:
-                cov = np.array([[1,1-self.noise],[1-self.noise,1]])
-            else:
-                cov = np.array([[1,-1+self.noise],[-1+self.noise,1]])
-            X[i,:] = self.rng.multivariate_normal([0,0], cov=cov)
-        return X
+    def __init__(self, outputs=None, inputs=None, noise=None, rng=None):
+        if rng is None:
+            rng = gen_rng(0)
+        if outputs is None:
+            outputs = [0, 1]
+        if noise is None:
+            noise = .1
+        self.rng = rng
+        self.outputs = outputs
+        self.inputs = []
+        self.noise = noise
 
-    def logpdf_xy(self, x, y):
+    def simulate(self, rowid, query, evidence=None):
+        if not evidence:
+            sample = self.simulate_joint()
+            return [sample[self.outputs.index(q)] for q in query]
+        else:
+            assert len(evidence) == len(query) == 1
+            z = evidence.values()[0]
+            return self.simulate_conditional(z)
+
+    def logpdf(self, rowid, query, evidence):
+        if not evidence:
+            if len(query) == 2:
+                x, y = query.values()
+                return self.logpdf_joint(x, y)
+            else:
+                z = query.values()[0]
+                return self.logpdf_maringal(z)
+        else:
+            assert len(evidence) == len(query) == 1
+            z = evidence.values()[0]
+            w = query.values()[0]
+            return self.logpdf_conditional(w, z)
+
+    # Internal simulators and assesors.
+
+
+    def simulate_joint(self):
+        if self.rng.rand() < .5:
+            cov = np.array([[1,1-self.noise],[1-self.noise,1]])
+        else:
+            cov = np.array([[1,-1+self.noise],[-1+self.noise,1]])
+        return self.rng.multivariate_normal([0,0], cov=cov)
+
+    def logpdf_joint(self, x, y):
         return logsumexp([
             np.log(.5)+multivariate_normal.logpdf([x,y], [0,0],
                 cov=[[1,1-self.noise],[1-self.noise,1]]),
@@ -44,14 +80,14 @@ class XCrossGpm(synthetic.SyntheticXyGpm):
                 cov=[[1,-1+self.noise],[-1+self.noise,1]]),
             ])
 
-    def logpdf_x(self, x):
-        return norm.logpdf(x, scale=1)
-
-    def logpdf_y(self, y):
-        return norm.logpdf(y, scale=1)
-
-    def logpdf_x_given_y(self, x, y):
+    def logpdf_marginal(self, z):
         raise NotImplementedError
 
-    def logpdf_y_given_x(self, y, x):
+    def logpdf_conditional(self, w, z):
+        raise NotImplementedError
+
+    def simulate_conditional(self, z):
+        raise NotImplementedError
+
+    def mutual_information(self):
         raise NotImplementedError
