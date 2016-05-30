157ce7ba403eda80b2f910f9e22baeffc11139c3
F Saad <fsaad@mit.edu>
2016-05-30 14:49:49 -0400

    Convert dots to CGPM.

--- src/uncorrelated/dots.py
+++ src/uncorrelated/dots.py
@@ -19,24 +19,51 @@ import numpy as np
 from scipy.misc import logsumexp
 from scipy.stats import norm
 
-from gpmcc.uncorrelated import synthetic
+
+from gpmcc.gpm import Gpm
+from gpmcc.utils.general import gen_rng
 
 
-class DotsGpm(synthetic.SyntheticXyGpm):
-    """(X,Y) ~ Four Dots"""
+class Dots(Gpm):
+    """(X,Y) ~ Four Dots."""
 
     mx = [ -1, 1, -1, 1]
     my = [ -1, -1, 1, 1]
 
+    def __init__(self, outputs=None, inputs=None, noise=None, rng=None):
+        if rng is None:
+            rng = gen_rng(0)
+        if outputs is None:
+            outputs = [0, 1]
+        if noise is None:
+            noise = .1
+        self.rng = rng
+        self.outputs = outputs
+        self.inputs = []
+        self.noise = noise
+
+    def simulate(self, rowid, query, evidence=None):
+        sample = self.simulate_xy(size=1)
+        return [sample[self.outputs.index(q)] for q in query]
+
+    def logpdf(self, rowid, query, evidence):
+        logp = 0
+        x = query.get(self.outputs[0], None)
+        y = query.get(self.outputs[1], None)
+        assert x or y
+        if x:
+            logp += self.logpdf_x(x)
+        if y:
+            logp += self.logpdf_y(y)
+        return logp
+
+    # Internal simulators and assesors.
+
     def simulate_xy(self, size=None):
-        X = np.zeros((size,2))
-        for i in xrange(size):
-            n = self.rng.randint(4)
-            x = self.rng.normal(loc=self.mx[n], scale=self.noise)
-            y = self.rng.normal(loc=self.my[n], scale=self.noise)
-            X[i,0] = x
-            X[i,1] = y
-        return X
+        n = self.rng.randint(4)
+        x = self.rng.normal(loc=self.mx[n], scale=self.noise)
+        y = self.rng.normal(loc=self.my[n], scale=self.noise)
+        return [x, y]
 
     def logpdf_xy(self, x, y):
         return logsumexp([np.log(.25)
@@ -45,18 +72,20 @@ class DotsGpm(synthetic.SyntheticXyGpm):
             for (mx,my) in zip(self.mx, self.my)])
 
     def logpdf_x(self, x):
-        return logsumexp([np.log(.5) + norm.logpdf(x, loc=mx, scale=self.noise)
+        return logsumexp(
+            [np.log(.5) + norm.logpdf(x, loc=mx, scale=self.noise)
             for mx in set(self.mx)])
 
     def logpdf_y(self, y):
-        return logsumexp([np.log(.5) + norm.logpdf(y, loc=my, scale=self.noise)
+        return logsumexp(
+            [np.log(.5) + norm.logpdf(y, loc=my, scale=self.noise)
             for my in set(self.my)])
 
     def logpdf_x_given_y(self, x, y):
-        raise NotImplementedError
+        return self.logpdf_x(x)
 
     def logpdf_y_given_x(self, y, x):
-        raise NotImplementedError
+        return self.logpdf_y(y)
 
     def mutual_information(self):
         samples = self.simulate_xy(size=1000)
