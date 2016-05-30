4e76c005b25f8fb6ad6af4f2c16d630ccf0f909c
F Saad <fsaad@mit.edu>
2016-05-30 14:28:26 -0400

    Convert sin to CGPM.

--- src/uncorrelated/diamond.py
+++ src/uncorrelated/diamond.py
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import numpy as np
-
-from scipy.misc import logsumexp
 from scipy.stats import uniform
 
 from gpmcc.gpm import Gpm
