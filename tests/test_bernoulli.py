d428b9a19efe463b2c5f79870ea6a87032d13ffc
F Saad <fsaad@mit.edu>
2016-06-18 16:23:37 -0400

    Update test bernoulli for b2be8d7.

--- tests/test_bernoulli.py
+++ tests/test_bernoulli.py
@@ -33,13 +33,16 @@ NUM_ITER = 5
 def test_bernoulli():
     # Switch for multithread (0 is faster).
     multithread = 0
+
     # Create categorical data of DATA_NUM_0 zeros and DATA_NUM_1 ones.
     data = np.transpose(np.array([[0] * DATA_NUM_0 + [1] * DATA_NUM_1]))
+
     # Run a single chain for a few iterations.
     engine = Engine(
         data, cctypes=['categorical'], distargs=[{'k': 2}],
         rng=gen_rng(0), multithread=0)
     engine.transition(NUM_ITER, multithread=multithread)
+
     # Simulate from hypothetical row and compute the proportion of ones.
     sample = engine.simulate(-1, [0], N=NUM_SIM, multithread=multithread)[0]
     sum_b = sum(s[0] for s in sample)
@@ -47,13 +50,17 @@ def test_bernoulli():
     true_prob_of_1 = float(DATA_NUM_1) / float(DATA_NUM_0 + DATA_NUM_1)
     # Check 1% relative match.
     assert np.allclose(true_prob_of_1, observed_prob_of_1, rtol=.1)
+
     # Simulate from observed row as a crash test.
     sample = engine.simulate(1, [0], N=1, multithread=multithread)
+
     # Ensure normalized unobserved probabilities.
     p0_uob = engine.logpdf(-1, {0:0}, multithread=multithread)
     p1_uob = engine.logpdf(-1, {0:1}, multithread=multithread)
     assert np.allclose(logsumexp([p0_uob, p1_uob]), 0)
+
     # Ensure normalized observed probabilities.
-    p0_obs = engine.logpdf(1, {0:0}, multithread=multithread)
-    p1_obs = engine.logpdf(1, {0:1}, multithread=multithread)
-    assert np.allclose(logsumexp([p0_obs, p1_obs]), 0)
+    # XXX These are disabled since cell 1 is not nan.
+    # p0_obs = engine.logpdf(1, {0:0}, multithread=multithread)
+    # p1_obs = engine.logpdf(1, {0:1}, multithread=multithread)
+    # assert np.allclose(logsumexp([p0_obs, p1_obs]), 0)
