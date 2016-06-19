2057e0e9a1362d5c50caa8021ecfbe3e1065a94c
F Saad <fsaad@mit.edu>
2016-06-19 08:52:36 -0400

    Dim documentation.

--- src/mixtures/dim.py
+++ src/mixtures/dim.py
@@ -58,45 +58,45 @@ class Dim(CGpm):
         rng : np.random.RandomState, optional.
             Source of entropy.
         """
+        # -- Seed --------------------------------------------------------------
         self.rng = gu.gen_rng() if rng is None else rng
 
-        # Outputs.
+        # -- Outputs -----------------------------------------------------------
         if len(outputs) != 1:
             raise ValueError('Dim requires exactly 1 output.')
         self.outputs = outputs
         self.inputs = inputs if inputs else []
         self.outputs = list(outputs)
 
-        # Inputs.
+        # -- Inputs ------------------------------------------------------------
         if len(inputs) < 1:
             raise ValueError('Dim requires at least 1 input.')
         self.inputs = list(inputs)
 
-        # Identifier. Find a better way to encapsulate.
+        # -- Identifier --------------------------------------------------------
         self.index = self.outputs[0]
 
-        # Model type.
+        # -- DistributionCGpms -------------------------------------------------
         self.model = cu.cctype_class(cctype)
         self.cctype = self.model.name()
         self.distargs = distargs if distargs is not None else {}
 
-        # Hyperparams.
+        # -- Hyperparameters ---------------------------------------------------
         self.hyper_grids = {}
         self.hypers = hypers if hypers is not None else {}
 
-        # Clusters.
+        # -- Clusters and Assignments ------------------------------------------
         self.clusters = {}  # Mapping of cluster k to the object.
         self.Zr = {}        # Mapping of non-nan rowids to cluster k.
         self.Zi = {}        # Mapping of nan rowids to cluster k.
 
-        # Auxiliary singleton model.
+        # -- Auxiliary Singleton ---- ------------------------------------------
         self.aux_model = self.create_aux_model()
 
     # --------------------------------------------------------------------------
     # Observe
 
     def incorporate(self, rowid, query, evidence=None):
-        """Record an observation."""
         if rowid in self.Zr or rowid in self.Zi:
             raise ValueError('rowid already incorporated: %d.' % rowid)
         k, evidence, valid = self.preprocess(query, evidence)
@@ -110,7 +110,6 @@ class Dim(CGpm):
             self.Zi[rowid] = k
 
     def unincorporate(self, rowid):
-        """Remove observation rowid."""
         if rowid in self.Zi:
             del self.Zi[rowid]
         elif rowid in self.Zr:
@@ -124,14 +123,12 @@ class Dim(CGpm):
     # logpdf score
 
     def logpdf_score(self):
-        """Return log score summed over all clusters."""
         return sum(self.clusters[k].logpdf_score() for k in self.clusters)
 
     # --------------------------------------------------------------------------
     # logpdf
 
     def logpdf(self, rowid, query, evidence=None):
-        """Evaluate the log density of the query given evidence."""
         k, evidence, valid = self.preprocess(query, evidence)
         cluster = self.clusters.get(k, self.aux_model)
         return cluster.logpdf(rowid, query, evidence) if valid else 0
@@ -140,7 +137,6 @@ class Dim(CGpm):
     # Simulate
 
     def simulate(self, rowid, query, evidence=None, N=None):
-        """Simulate the query given evidence."""
         k, evidence, valid = self.preprocess(query, evidence)
         if not valid:
             raise ValueError('Bad simulate args: %s, %s.') % (query, evidence)
@@ -151,13 +147,13 @@ class Dim(CGpm):
     # Inferece
 
     def transition_params(self):
-        """Updates the component parameters of each cluster."""
+        """Transitions the component parameters of each cluster."""
         if not self.is_collapsed():
             for k in self.clusters:
                 self.clusters[k].transition_params()
 
     def transition_hypers(self):
-        """Updates the hyperparameters of each cluster."""
+        """Transitions the hyperparameters of each cluster."""
         def transition_hyper(target):
             def compute_cluster_proposal(k, g):
                 self.hypers[target] = g
@@ -178,7 +174,7 @@ class Dim(CGpm):
         self.aux_model = self.create_aux_model()
 
     def transition_hyper_grids(self, X, n_grid=30):
-        """Resample the hyperparameter grids using empirical Bayes."""
+        """Transitions hyperparameter grids using empirical Bayes."""
         self.hyper_grids = self.model.construct_hyper_grids(
             [x for x in X if not math.isnan(x)], n_grid=n_grid)
         # Only transition the hypers if previously uninstantiated.
@@ -188,28 +184,31 @@ class Dim(CGpm):
         self.aux_model = self.create_aux_model()
 
     # --------------------------------------------------------------------------
-    # Helpers
-
-    def is_collapsed(self):
-        return self.model.is_collapsed()
-
-    def is_continuous(self):
-        return self.model.is_continuous()
-
-    def is_conditional(self):
-        return self.model.is_conditional()
-
-    def is_numeric(self):
-        return self.model.is_numeric()
+    # Attributes from self.model
 
     def get_distargs(self):
         return self.aux_model.get_distargs()
 
+    def is_collapsed(self):
+        return self.model.is_collapsed()
+
+    def is_conditional(self):
+        return self.model.is_conditional()
+
+    def is_continuous(self):
+        return self.model.is_continuous()
+
+    def is_numeric(self):
+        return self.model.is_numeric()
+
     def name(self):
         return self.aux_model.get_name()
 
+    # --------------------------------------------------------------------------
+    # Plotter
+
     def plot_dist(self, X, Y=None, ax=None):
-        """Plots the predictive distribution and histogram of X."""
+        """Plots predictive distribution and a histogram of data X."""
         plotter = pu.plot_dist_continuous if self.model.is_continuous() else \
             pu.plot_dist_discrete
         return plotter(
