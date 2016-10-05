bc53f703f054a7b1706075317fe703d46db1655d
leocasarsa <leonardocazevedo@gmail.com>
2016-10-04 22:43:26 -0400

    Added MWE for BUG: view.incorporate does not accept missing columns.

---
+++ tests/MWE_view_incorporate_missing_value.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2016 MIT Probabilistic Computing Project
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This test suite ensures that results returned from View.logpdf_multirow,
+State.logpdf_multirow and Engine.logpdf_multirow, are analytically correct
+(for some cases) and follow the rules of probability. 
+"""
+
+import pytest
+import numpy as np
+
+from cgpm.utils import general as gu
+from cgpm.mixtures.view import View
+
+@pytest.fixture
+def priorCGPM():
+    data = np.random.choice([0, 1], size=(100, 5))
+    outputs = range(5)
+    X = {c: data[:, i].tolist() for i, c in enumerate(outputs)}
+    model = View(
+        X,
+        cctypes=['bernoulli']*5,
+        outputs=[1000] + outputs,
+        rng=gu.gen_rng(0))
+    return model
+
+def test_view_incorporate_missing_value(priorCGPM):
+    view = priorCGPM
+
+    # incorporate full row and sample cluster (works)
+    normal_row = {i: 1 for i in range(5)}
+    view.incorporate(42000, query=normal_row)
+    view.unincorporate(42000)
+
+    # incorporate full row and force cluster (works)
+    forced_row = {i: 1 for i in range(5) + [view.outputs[0]]}
+    view.incorporate(42001, query=forced_row)
+    view.unincorporate(42001)
+
+    # incorporate row with missing values (fails)
+    missing_row = {i: 1 for i in range(3)}
+    view.incorporate(42002, query=missing_row)
+    view.unincorporate(42002)
