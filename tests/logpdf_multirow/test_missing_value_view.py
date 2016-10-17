9680e94b18d5155b0776e07956a8b6d80700da89
leocasarsa <leonardocazevedo@gmail.com>
2016-10-17 11:53:21 -0400

    test missing values crashing where they are expected to.

--- tests/logpdf_multirow/test_missing_value_view.py
+++ tests/logpdf_multirow/test_missing_value_view.py
@@ -19,42 +19,97 @@ import numpy as np
 
 from cgpm.utils import general as gu
 from cgpm.mixtures.view import View
+from cgpm.utils.general import logsumexp
 
-@pytest.fixture
-def priorCGPM():
-    data = np.random.choice([0, 1], size=(100, 5))
-    outputs = range(5)
+@pytest.fixture(params=[(seed, D, do_analyze) 
+                        for seed in range(1) 
+                        for D in [2]
+                        for do_analyze in [False]])
+def exampleCGPM(request):
+    seed, D, do_analyze = request.param
+
+    rng = gu.gen_rng(seed)
+    data = rng.choice([0, 1], size=(100, D))
+    outputs = range(D)
     X = {c: data[:, i].tolist() for i, c in enumerate(outputs)}
-    model = View(
+    view = View(
         X,
-        cctypes=['bernoulli']*5,
+        cctypes=['bernoulli']*D,
         outputs=[1000] + outputs,
-        rng=gu.gen_rng(0))
-    return model
+        rng=rng)
 
-def test_crash_incorporate_missing_value(priorCGPM):
-    view = priorCGPM
+    view.seed = seed
+    view.D = D
+    if do_analyze:
+        view.analyze(N=30)
+    return view
+ 
+def test_crash_incorporate_missing_value(exampleCGPM):
+    view = exampleCGPM
 
-    # incorporate full row and sample cluster (works)
-    normal_row = {i: 1 for i in range(5)}
-    view.incorporate(42000, query=normal_row)
-    view.unincorporate(42000)
+    if view.D == 2:
+        # 1. incorporate full row and sample cluster (works)
+        normal_row = {i: 1 for i in range(2)}
+        view.incorporate(42000, query=normal_row)
+        view.unincorporate(42000)
 
-    # incorporate full row and force cluster (works)
-    forced_row = {i: 1 for i in range(5) + [view.outputs[0]]}
-    view.incorporate(42001, query=forced_row)
-    view.unincorporate(42001)
+        # 2. incorporate full row and force cluster (works)
+        forced_row = {i: 1 for i in range(2) + [view.outputs[0]]}
+        view.incorporate(42001, query=forced_row)
+        view.unincorporate(42001)
 
-    # incorporate row with missing values (fails)
-    missing_row = {i: 1 for i in range(3)}
-    view.incorporate(42002, query=missing_row)
-    view.unincorporate(42002)
+        # 3. incorporate row with missing value (fails)
+        missing_row = {0: 1}
+        view.incorporate(42002, query=missing_row)
+        view.unincorporate(42002)
 
-def test_logpdf_missing_value(priorCGPM):
-    raise NotImplementedError
+# view.logpdf already accepts query with missing values
+# def test_logpdf_missing_value(exampleCGPM):
+#     view = exampleCGPM
+ 
+#     if view.D == 2:
+#         # 1. Test crash for missing value logpdf
+#         marg_query = {0: 1}
+#         marg_logpdf = view.logpdf(-1, query=marg_query)  # should crash
+#         assert False, "marg_logpdf should have crashed"
 
-def test_logpdf_multirow_missing_values(priorCGPM):
-    raise NotImplementedError
+#         # 2. Test missing value logpdf makes sense
+#         joint_queries = [{0: 1, 1: i} for i in range(2)]
+#         joint_logpdfs = [view.logpdf(-1, query=joint_queries[i])
+#                          for i in range(2)]
+#         marg_joint_logpdf = logsumexp(joint_logpdfs)
 
-def test_simulate_missing_values():
+#         assert np.allclose(marg_logpdf, marg_joint_logpdf, atol=.1)
+
+#     else:
+#         raise Exception("No test run")
+
+def test_logpdf_multirow_missing_value(exampleCGPM):
+    view = exampleCGPM
+    
+    if view.D == 2:
+        # 0. Missing value in query should run
+        marg_query = {0: 1}
+        evid = {0: {0: 1, 1: 1}}
+        lp0 = view.logpdf_multirow(
+            -1, query=marg_query, evidence=evid)
+
+        # 1. Test crash for missing value in evidence
+        query = {0: 1, 1: 1}
+        marg_evid = {0: {0: 1}}
+        lp1 = view.logpdf_multirow(
+            -1, query=query, evidence=marg_evid)
+        assert False, "should have crashed"
+
+        # 2. Test missing value logpdf makes sense
+        joint_queries = [{0: 1, 1: i} for i in range(2)]
+        joint_logpdfs = [view.logpdf_multirow(
+            -1, query=joint_queries[i], evidence=marg_evid) for i in range(2)]
+        lp2 = logsumexp(joint_logpdfs)
+        assert np.allclose(lp1, lp2, atol=.1)
+
+    else:
+        raise Exception("No test run")
+
+def test_simulate_missing_value():
     raise NotImplementedError
