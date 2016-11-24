e32275b7b6f54f9561d607cc1a6acf3757f7b39c
Leo Casarsa <casarsa@probcomp-4.csail.mit.edu>
2016-11-24 05:52:44 -0500

    TEST FOR BUG: utils.general.merged does not merge nested dictionary.

---
+++ tests/test_debug_20161124.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2016 MIT Probabilistic Computing Project
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tests for debugging subroutines in logpdf_multirow.
+"""
+import numpy as np
+from itertools import product
+
+from cgpm.mixtures.view import View
+from cgpm.utils.general import logsumexp, merged
+
+def initialize_view():
+    data = np.array([[1, 1]])
+    D = len(data[0])
+    outputs = range(D)
+    X = {c: data[:, i].tolist() for i, c in enumerate(outputs)}
+    view = View(
+        X,
+        outputs=[1000] + outputs,
+        alpha=1.,
+        cctypes=['bernoulli']*D,
+        hypers={
+            i: {'alpha': 1., 'beta': 1.} for i in outputs},
+        Zr=[0])
+    return view
+
+def test_merged():
+    query = {0: {0: 1}, 1: {0: 1}}
+    evidence = {0: {1000: 1}, 1: {1000: 1}}
+    joint_input = {0: {0: 1, 1000: 1},
+                   1: {0: 1, 1000: 1}}
+    
+    merged_input = merged([query, evidence])
+    assert joint_input == merged_input
