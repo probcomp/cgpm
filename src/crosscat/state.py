bfaf4b0f78350b0d0b7933407174ed2a7a7fe28c
Leo Casarsa <casarsa@probcomp-4.csail.mit.edu>
2016-11-28 20:09:18 -0500

    added state.relevance_score, state.relevance_search. Tests pass.

--- src/crosscat/state.py
+++ src/crosscat/state.py
@@ -284,6 +284,21 @@ class State(CGpm):
         return network.logpdf(rowid, query, evidence)
 
     # --------------------------------------------------------------------------
+    # relevance score
+    def relevance_score(self, query, evidence, context, debug=False):
+        """
+        Compute the relevance score of query wrt evidence in the view
+        of variable context.
+        """
+        context_view = self.Zv()[context]
+        return self.views[context_view].relevance_score(
+            query, evidence, debug)
+
+    def relevance_search(self, evidence, context, debug=False):
+        context_view = self.Zv()[context]
+        return self.views[context_view].relevance_search(
+            evidence, debug)
+    # --------------------------------------------------------------------------
     # Simulate
 
     def simulate(self, rowid, query, evidence=None, N=None, accuracy=None):
