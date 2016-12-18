d47d4193940cbaccc9381f773afc439d648748d5
Leo Casarsa <casarsa@probcomp-4.csail.mit.edu>
2016-12-18 00:25:16 -0500

    BUG: when computing relevance search in the iris_pc, it tries to incorporate a previously unincorporated row into new rowid.

---
+++ tests/test_relevance_search_numerical.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from cgpm.mixtures.view import View
+
+def load_csv():
+    df = pd.read_csv('tests/resources/iris_pc_noname.csv')
+    return df
+
+def load_view():
+    df = load_csv()
+    X = {c: df[['flower_pc1', 'flower_pc2']].values[:, c] for c in range(2)}
+    Zr = df['class']
+    outputs = [1000, 0, 1]
+    view = View(X, outputs, cctypes=['normal', 'normal'], Zr=Zr)
+    
+    return view
+
+def test_search_row_146():
+    view = load_view()
+
+    query = {146: {}}
+    import pudb; pudb.set_trace()
+    view.relevance_search(query)
+    pass
