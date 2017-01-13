ef62efb6d1a23744e9bc3ead40cbbb1059686832
Leo Casarsa <casarsa@probcomp-4.csail.mit.edu>
2017-01-13 15:55:28 -0500

    refactored tests to improve scalability

--- tests/test_logpdf_multirow_view.py
+++ tests/test_logpdf_multirow_view.py
@@ -26,8 +26,7 @@ from itertools import product
 from cgpm.mixtures.view import View
 from cgpm.utils.general import logsumexp, deep_merged
 
-# TODO: BUILD TESTS UPON EACH OTHER
-
+# # Helper Functions 
 def initialize_view():
     data = np.array([[1, 1]])
     D = len(data[0])
@@ -43,176 +42,113 @@ def initialize_view():
         Zr=[0])
     return view
 
-def test_logpdf_multirow_in_singlerow_query_hypothetical():
+def check_logpdf_multirow_answer(answer, query, evidence=None):
     view = initialize_view()
+    
+    l = view.logpdf_multirow(query, evidence, debug=True)
+    assert np.allclose(answer, l)
 
-    # LOGPDF
-    # P(x[1,0] = 1) = 7./12
-    # Hypothetical row: rowid=1
-    query = {0: 1}
-    math_out = np.log(7./12)
-    test_out = view.logpdf(rowid=1, query=query)
-    assert np.allclose(math_out, test_out)
+# # Static Variables
+Z = initialize_view().exposed_latent
 
-    # LOGPDF MULTIROW
+# TEST MARGINAL
+
+def test_marginal_one_hypothetical_query_row():
     # P(x[1,0] = 1) = 7./12
-    # Hypothetical row: rowid=1
     query = {1: {0: 1}}
-    math_out = np.log(7./12)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(7./12)
+    check_logpdf_multirow_answer(answer, query)
 
-def test_logpdf_multirow_in_singlerow_query_nonhypothetical():
-    view = initialize_view()
-
-    # LOGPDF
-    # P(x[0,0] = 1) = 1./2
-    # Non-hypothetical row: rowid=0
-    query = {0: 1}
-    math_out = np.log(1./2)
-    test_out = view.logpdf(rowid=0, query=query)
-    # assert np.allclose(math_out, test_out)  # FAILING
-
-    # LOGPDF MULTIROW
+def test_marginal_one_nonhypothetical_query_row():
     # P(x[0,0] = 1) = 2./3
-    # Non-hypothetical row: rowid=0
     query = {0: {0: 1}}
-    math_out = np.log(1./2)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(1./2)
+    check_logpdf_multirow_answer(answer, query)
 
-def test_logpdf_multirow_in_singlerow_cluster_hypothetical():
-    view = initialize_view()
+def test_marginal_one_hypothetical_cluster():
+    # P(Z[1] = 0) = 1./2
+    query = {1: {Z: 0}}
+    answer = np.log(1./2)
+    check_logpdf_multirow_answer(answer, query)
 
-    # LOGPDF
-    # P(z[1] = 0) = 1./2
-    # Hypothetical row: rowid=1
-    query = {view.exposed_latent: 0}
-    math_out = np.log(1./2)
-    test_out = view.logpdf(rowid=1, query=query)
-    assert np.allclose(math_out, test_out)
+def test_marginal_one_nonhypothetical_cluster():
+    # P(Z[0] = 0) = 1.
+    query = {0: {Z: 0}}
+    answer = np.log(1)
+    check_logpdf_multirow_answer(answer, query)
 
-    # LOGPDF MULTIROW
-    # P(z[1] = 0) = 1./2
-    # Hypothetical row: rowid=1
-    query = {1: {view.exposed_latent: 0}}
-    math_out = np.log(1./2)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)  # FAILS: math_out == 0
+# TEST JOINT DENSITY 
 
-def test_logpdf_multirow_in_singlerow_cluster_nonhypothetical():
-    view = initialize_view()
-
-    # LOGPDF
-    # P(z[0] = 0) = 1.
-    # Non-hypothetical row: rowid=0
-    query = {view.exposed_latent: 0}
-    math_out = np.log(1)
-    test_out = view.logpdf(rowid=0, query=query)
-    assert np.allclose(math_out, test_out)
-
-    # LOGPDF MULTIROW
-    # P(z[0] = 0) = 1.
-    # Non-hypothetical row: rowid=0
-    query = {0: {view.exposed_latent: 0}}
-    math_out = np.log(1)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
-
-def test_joint_logpdf_multirow_in_one_column():
-    view = initialize_view()
-
+def test_joint_two_rows_one_column():
     # P(x[0,0] = 1, x[1,0] = 1) = 7./24
     # (= 1./2 * 1 * (2./3 * 1./2 + 1./2 * 1./2)
     # Missing column and non-hypothetical row
     query = {0: {0: 1}, 1: {0: 1}}
-    math_out = np.log(7./24)
+    answer = np.log(7./24)
+    check_logpdf_multirow_answer(answer, query)
 
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
-
-def test_joint_logpdf_multirow_in_two_columns_with_missing_values():
-    view = initialize_view()
-
-    # P(x[0,0] = 1, x[1,1]=1)
-    # = 1./2 * 1 * (1./2 * 1./2 + 1./2 * 1./2)
-    query = {0: {0: 1}, 1: {1: 1}}
-    math_out = np.log(1./4)
-
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
-
-def test_joint_logpdf_multirow_in_one_column_with_cluster_assignments():
-    view = initialize_view()
-
-    # P(row 0: {0: 1, z: 0}, row 1: {0: 1, z: 1}) = 1./8
-    # 1./2 * 1 * (1./2 * 1./2)
-    z = view.exposed_latent
-    query = {0: {0: 1, z: 0}, 1: {0: 1, z: 1}}
-    math_out = np.log(1./8)
-
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
-
-def test_joint_logpdf_multirow_in_one_column_conditioned_on_cluster_assignments():
-    view = initialize_view()
-
-    # p({row 0: {0: 1}, row 1: {0: 1}} | {0: {z: 0}, 1: {z: 1}}) = 1./4
-    # = 1./8 * 1./(1./2)
-    z = view.exposed_latent
-    query = {0: {0: 1}, 1: {0: 1}}
-    evidence = {0: {z: 0}, 1: {z: 1}}
-    math_out = np.log(1./4)
-
-    test_out = view.logpdf_multirow(query=query, evidence=evidence, debug=True)
-    assert np.allclose(math_out, test_out)
-
-def test_joint_logpdf_multirow_in_two_columns():
-    view = initialize_view()
-
+def test_joint_two_rows_two_columns():
     # P(x[0,:] = [1,1], x[1,:] = [1,1]) = 25./288
     query = {0: {0: 1, 1: 1},
              1: {0: 1, 1: 1}}
-    math_out = np.log(25./288)
+    answer = np.log(25./288)
+    check_logpdf_multirow_answer(answer, query)
 
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
+def test_joint_two_rows_two_columns_missing_values():
+    # P(x[0,0] = 1, x[1,1]=1)
+    # = 1./2 * 1 * (1./2 * 1./2 + 1./2 * 1./2)
+    query = {0: {0: 1}, 1: {1: 1}}
+    answer = np.log(1./4)
+    check_logpdf_multirow_answer(answer, query)
 
-def test_logpdf_multirow_in_one_column_conditioned_on_another_row():
-    view = initialize_view()
+def test_joint_two_rows_with_clusters():
+    # P(row 0: {0: 1, Z: 0}, row 1: {0: 1, Z: 1}) = 1./8
+    # 1./2 * 1 * (1./2 * 1./2)
+    query = {0: {0: 1, Z: 0}, 1: {0: 1, Z: 1}}
+    answer = np.log(1./8)
+    check_logpdf_multirow_answer(answer, query)
 
+# TEST CONDITIONAL DENSITY
+
+def test_conditional_two_rows_given_clusters():
+    # p({row 0: {0: 1}, row 1: {0: 1}} | {0: {Z: 0}, 1: {Z: 1}}) = 1./4
+    # = 1./8 * 1./(1./2)
+    query = {0: {0: 1}, 1: {0: 1}}
+    evidence = {0: {Z: 0}, 1: {Z: 1}}
+    answer = np.log(1./4)
+    check_logpdf_multirow_answer(answer, query, evidence)
+
+def test_conditional_one_column_one_row_given_another_row_first_part():
     # P(x[1,0] = 1 | x[0,0] = 1) = 7./12
     # Missing column and non-hypothetical row
     query = {1: {0: 1}}
     evidence = {0: {0: 1}}
-    math_out = np.log(7./12)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(7./12)
+    check_logpdf_multirow_answer(answer, query, evidence)
 
+def test_conditional_one_column_one_row_given_another_row_second_part():
     # P(x[0,0] = 1 | x[1,0] = 1) = 7./12
     # Missing column and non-hypothetical row
     query = {0: {0: 1}}
     evidence = {1: {0: 1}}
-    math_out = np.log(7./12)
-    test_out = view.logpdf_multirow(query=query, evidence=evidence, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(7./12)
+    check_logpdf_multirow_answer(answer, query, evidence)
 
-def test_logpdf_multirow_in_two_columns_conditioned_on_another_row():
-    view = initialize_view()
-
+def test_conditional_two_columns_one_row_given_another_row_first_part():
     # P(x[1,:] = [1,1] | x[0,:] = [1,1]) = 25./72
     query = {1: {0: 1, 1: 1}}
     evidence = {0: {0: 1, 1: 1}}
-    math_out = np.log(25./72)
-    test_out = view.logpdf_multirow(query=query, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(25./72)
+    check_logpdf_multirow_answer(answer, query, evidence)
 
+def test_conditional_two_columns_one_row_given_another_row_second_part():
     # P(x[0,:] = [1,1] | x[1,:] = [1,1]) = 25./72
     query = {0: {0: 1, 1: 1}}
     evidence = {1: {0: 1, 1: 1}}
-    math_out = np.log(25./72)
-    test_out = view.logpdf_multirow(query=query, evidence=evidence, debug=True)
-    assert np.allclose(math_out, test_out)
+    answer = np.log(25./72)
+    check_logpdf_multirow_answer(answer, query, evidence)
+
+# TEST PRODUCT RULE
 
 def test_factorization_of_joint_logpdf_multirow_in_two_columns():
     """
@@ -229,6 +165,8 @@ def test_factorization_of_joint_logpdf_multirow_in_two_columns():
     log_marginal = view.logpdf_multirow(query=row0)
 
     assert np.allclose(log_joint, log_conditional + log_marginal)
+
+# TEST MARGINALIZATION
 
 def test_marginalization_of_joint_logpdf_in_two_columns():
     """
@@ -267,6 +205,8 @@ def test_marginalization_of_joint_logpdf_in_one_column():
             [log_marginalized_joint, log_joint])
 
     assert np.allclose(log_marginal, log_marginalized_joint)
+
+# TEST BAYES RULE
 
 def test_bayes_inversion_of_logpdf_multirow_in_two_columns():
     """
@@ -330,10 +270,10 @@ def test_same_rowid_of_query_and_evidence_hypothetical():
     # P({1: {0: 1}} | {1: {0: 1}}) hypothetical row
     row1 = {1: {0: 1}}
     row2 = {2: {0: 1}}
-    math_out = view.logpdf_multirow(query=row2, evidence=row1)
+    answer = view.logpdf_multirow(query=row2, evidence=row1)
     with pytest.warns(UserWarning):
         test_out = view.logpdf_multirow(query=row1.copy(), evidence=row1)
-    assert np.allclose(math_out, test_out)
+    assert np.allclose(answer, test_out)
 
 def test_same_rowid_of_query_and_evidence_nonhypothetical():
     view = initialize_view()
@@ -341,39 +281,13 @@ def test_same_rowid_of_query_and_evidence_nonhypothetical():
     # P({0: {0:1} | {0: {0: 1}) non-hypothetical row
     row0 = {0: {0: 1}}
     row1 = {1: {0: 1}}
-    math_out = view.logpdf_multirow(query=row1, evidence=row0)
+    answer = view.logpdf_multirow(query=row1, evidence=row0)
     with pytest.warns(UserWarning):
         test_out = view.logpdf_multirow(query=row0.copy(), evidence=row0)
-    assert np.allclose(math_out, test_out)
+    assert np.allclose(answer, test_out)
 
- # TEST CONCORDANCE WITH LOGPDF
- # Deactivated, logpdf works differently for non-hypothetical rows
-# def test_logpdf_in_test_joint_logpdf_factorizes():
-#     view = initialize_view()
-#     row0 = {0: {0: 1, 1: 1}}
 
-#     log_marginal = view.logpdf_multirow(query=row0)
-#     assert np.allclose(log_marginal, view.logpdf(rowid=0, query=row0[0]))
 
-# def test_logpdf_in_test_joint_logpdf_marginalizes():
-#     view = initialize_view()
-#     row1 = {1: {0: 0, 1: 0}}
-#     log_marginal = view.logpdf_multirow(query=row1)  # log_p(row1)
-#     assert np.allclose(log_marginal, view.logpdf(rowid=1, query=row1[1]))
 
-# def test_logpdf_in_test_bayes_inversion():
-#     """
-#     p(row 1 | row 2) = p(row 2| row 1) p(row 1) / p(row 2)
-#     log_p(row 1 | row 2) = log_p(row 2| row 1) + log_p(row 1) - log_p(row 2)
-#     log_posterior = log_likelihood + log_prior - log_marginal
-#     """
-#     view = initialize_view()
-#     row1 = {1: {0: 0, 1: 0}}
-#     row0 = {0: {0: 1, 1: 1}}
 
-#     log_prior = view.logpdf_multirow(query=row1)
-#     assert np.allclose(log_prior, view.logpdf(rowid=1, query=row1[1]))
-
-#     log_marginal = view.logpdf_multirow(query=row0)
-#     assert np.allclose(log_marginal, view.logpdf(rowid=0, query=row0[0]))
 
