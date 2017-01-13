a10b60af68eff3536e19ac9888aa9ff8d8d1fdf8
Leo Casarsa <casarsa@probcomp-4.csail.mit.edu>
2017-01-13 18:26:18 -0500

    added two test functions for the posterior relevance score (still crashes)

--- tests/test_posterior_relevance_score.py
+++ tests/test_posterior_relevance_score.py
@@ -34,13 +34,49 @@ def initialize_view():
         Zr=[0])
     return view
 
+Z = initialize_view().exposed_latent
+
 def check_posterior_score_answer(answer, target_row, query_row_set):
     view = initialize_view()
     s = view.posterior_relevance_score(target_row, query_row_set)
     assert np.allclose(answer, s)
 
+def logsumexp_conditional_densities(num_of_clusters, target, query):
+    # logsumexp_k logpdf(clusters = k | target, query)
+    view = initialize_view()
+    evidence = deep_merged(target, query)
+    rowids = evidence.keys()
+    
+    s = -np.inf
+    for k in xrange(num_of_clusters+1):
+        query = {r: {Z: k} for r in rowids}
+        l = view.logpdf_multirow(query, evidence)
+        s = logsumexp((s, l))
+    
+    return s
+    
 def test_one_hypothetical_one_nonhypothetical_rows():
     # score({0: {0: 1}}; {1: {0: 1}}) = 24./56
     target = {0: {0: 1}}
     query = {1: {0: 1}}
     check_posterior_score_answer(24./56, target, query)
+
+def test_two_hypothetical_rows():
+    # score({1: {0: 1}}; {2: {0: 1}})
+    target = {1: {0: 1}}
+    query = {2: {0: 1}}
+    
+    # compute answer based on logpdf_multirow
+    answer = logsumexp_conditional_densities(1, target, query)
+    check_posterior_score_answer(answer, target, query)
+
+def test_three_hypothetical_rows():
+    # score({1: {0: 1}}; {2: {0: 1}, 3: {0: 1}})
+    target = {1: {0: 1}}
+    query = {2: {0: 1}, 3: {0: 1}}
+
+    # compute answer based on logpdf_multirow
+    num = logsumexp_conditional_densities(1, target, query)
+    den = logsumexp_conditional_densities(1, query, {})
+    answer = num * 1. / den
+    check_posterior_score_answer(answer, target, query)
