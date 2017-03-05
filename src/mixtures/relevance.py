12f14393aec7edfcba63c1cedbe5c61335ba99bb
F Saad <fsaad@mit.edu>
2017-03-05 11:14:20 -0500

    WIP for optimizing relevance score, with minor cleanups.
    TODO: push computation of existing crp and cluser probabilities outside
    the loop.

--- src/mixtures/relevance.py
+++ src/mixtures/relevance.py
@@ -113,10 +113,6 @@ def relevance_probability(view, rowid_target, rowid_query):
     for rowid_q in rowid_query:
         view.unincorporate(rowid_q)
 
-    # Marginal likelihood after unincorporating target and query, from which we
-    # subtract all marginal likelihoods.
-    logpdf_score_reference = view.logpdf_score()
-
     # Retrieve current tables.
     tables_crp = sorted(view.crp.clusters[0].counts)
 
@@ -212,12 +208,9 @@ def logpdf_assignments_marginalize_target(
     """Compute the joint probability of crp assignment and data for the
     query rows, marginalizing over the assignment of the target row."""
 
-    print 'getting view logpdf_score initial'
+    # Compute initial logpdf scores.
     logpdf_view_0 = view.logpdf_score()
-    # Compute the initial logpdf.
-    logpdf_crp_0 = view.crp.logpdf_score()
-    logpdf_query_0 = get_cluster_logpdf_score(view, table_query)
-    logpdf_initial = logpdf_crp_0 + logpdf_query_0
+    logpdf_clusters_0 = get_view_logpdf_score(view, table_query, table_query)
 
     # Incorporate the query rows.
     for rowid_q, values_q in zip(rowid_query, values_query):
@@ -225,17 +218,14 @@ def logpdf_assignments_marginalize_target(
         view.incorporate(rowid_q, values_q)
         del values_q[view.outputs[0]]
 
-    # Compute the new values.
-    logpdf_crp_1 = view.crp.logpdf_score()
-    logpdf_query_1 = get_cluster_logpdf_score(view, table_query)
-    logpdf_final = logpdf_crp_1 + logpdf_query_1
-
     # Compute new marginal likelihood.
-    logpdf_score = view.logpdf_score()
+    logpdf_view_1 = view.logpdf_score()
+    logpdf_clusters_1 = get_view_logpdf_score(view, table_query, table_query)
 
-    logpdf_delta = logpdf_final - logpdf_initial
-
-    assert np.allclose(logpdf_score - logpdf_view_0, logpdf_delta)
+    # Compute delta marginal likelihood.
+    logpdf_delta_clusters = logpdf_clusters_1 - logpdf_clusters_0
+    logpdf_delta_view = logpdf_view_1 - logpdf_view_0
+    assert np.allclose(logpdf_delta_view, logpdf_delta_clusters)
 
     # Compute predictive probability of target (marginalizes over tables).
     logpdf_predictive = view.logpdf(-1, values_target)
@@ -243,61 +233,45 @@ def logpdf_assignments_marginalize_target(
     for rowid_q in rowid_query:
         view.unincorporate(rowid_q)
     # Predictive joint probability (see docstring).
-    return logpdf_predictive + logpdf_delta
+    return logpdf_predictive + logpdf_delta_clusters
 
 
 def logpdf_assignments(
         view, rowid_target, rowid_query, values_target, values_query,
         table_target, table_query):
     """Compute the joint probability of crp assignment and data."""
-    print 'inside inner loop of logpdf_assignments'
+
     # Compute initial logpdf scores.
-    print 'getting view logpdf_score initial'
     logpdf_view_0 = view.logpdf_score()
-    print 'getting cluster logpdf score initial'
-    if table_target == table_query:
-        logpdf_crp_0 = view.crp.logpdf_score()
-        logpdf_target_0 = get_cluster_logpdf_score(view, table_target)
-        # logpdf_query_0 = get_cluster_logpdf_score(view, table_query)
-        logpdf_initial = logpdf_crp_0 + logpdf_target_0
-    else:
-        logpdf_crp_0 = view.crp.logpdf_score()
-        logpdf_target_0 = get_cluster_logpdf_score(view, table_target)
-        logpdf_query_0 = get_cluster_logpdf_score(view, table_query)
-        logpdf_initial = logpdf_crp_0 + logpdf_target_0 + logpdf_query_0
+    logpdf_clusters_0 = get_view_logpdf_score(view, table_target, table_query)
+
     # Incorporate target row.
     values_target[view.outputs[0]] = table_target
     view.incorporate(rowid_target, values_target)
     del values_target[view.outputs[0]]
+
     # Incorporate query rows.
     for rowid_q, row_values_q in zip(rowid_query, values_query):
         row_values_q[view.outputs[0]] = table_query
         view.incorporate(rowid_q, row_values_q)
         del row_values_q[view.outputs[0]]
+
     # Compute new marginal likelihood.
-    logpdf_score = view.logpdf_score()
-    # Compute final logpdf scores.
-    print 'getting view logpdf_score final'
     logpdf_view_1 = view.logpdf_score()
-    print 'getting view cluster score final'
-    if table_target == table_query:
-        logpdf_crp_1 = view.crp.logpdf_score()
-        logpdf_target_1 = get_cluster_logpdf_score(view, table_target)
-        # logpdf_query_1 = get_cluster_logpdf_score(view, table_query)
-        logpdf_final = logpdf_crp_1 + logpdf_target_1
-    else:
-        logpdf_crp_1 = view.crp.logpdf_score()
-        logpdf_target_1 = get_cluster_logpdf_score(view, table_target)
-        logpdf_query_1 = get_cluster_logpdf_score(view, table_query)
-        logpdf_final = logpdf_crp_1 + logpdf_target_1 + logpdf_query_1
-    logpdf_delta = logpdf_final - logpdf_initial
-    assert np.allclose(logpdf_view_1 - logpdf_view_0, logpdf_delta)
+    logpdf_clusters_1 = get_view_logpdf_score(view, table_target, table_query)
+
+    # Compute delta marginal likelihood.
+    logpdf_delta_clusters = logpdf_clusters_1 - logpdf_clusters_0
+    logpdf_delta_view = logpdf_view_1 - logpdf_view_0
+    assert np.allclose(logpdf_delta_view, logpdf_delta_clusters)
+
     # Unincorporate the target and query rows.
     view.unincorporate(rowid_target)
     for rowid_q in rowid_query:
         view.unincorporate(rowid_q)
+
     # Predictive joint probability is difference in marginal likelihood.
-    return logpdf_delta
+    return logpdf_delta_clusters
 
 
 def row_values(view, rowid):
@@ -323,11 +297,17 @@ def get_tables_different(tables):
     return tables_target, tables_query
 
 
+def get_view_logpdf_score(view, table_target, table_query):
+    """Retrieve logpdf score restricted to the crp and specified clusters."""
+    logpdf_crp = view.crp.logpdf_score()
+    logpdf_clusters = get_cluster_logpdf_score(view, table_query)
+    if table_target != table_query:
+        logpdf_clusters += get_cluster_logpdf_score(view, table_target)
+    return logpdf_crp + logpdf_clusters
+
+
 def get_cluster_logpdf_score(view, k):
-    """Return marginal likelihood of cluster k in View."""
-    if k in view.crp.clusters[0].counts:
-        dim_logps = [d.clusters[k].logpdf_score() for d in view.dims.itervalues()]
-    else: # We are in the aux model, which cannot have data!
-        dim_logps = [0]
-    print dim_logps
-    return sum(dim_logps)
+    """Return marginal likelihood of cluster k in View (0 for fresh cluster)."""
+    return sum([
+        d.clusters[k].logpdf_score() for d in view.dims.itervalues()
+    ]) if k in view.crp.clusters[0].counts else 0
