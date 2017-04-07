7a0bad73611072cb21933294d0ce4e17bb34226a
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2017-04-07 15:11:45 -0400

    Add tests for logpdf for k_means.
    We are testing logpdf for the following cases:
    
    	- k=1, unconditional
    	- k=2, unconditional
    	- k=1, conditional
    	- k=2, conditional
    
    All test cases crash where they are expected to (with NotImplementedErrorsr)
    in k_means.py. We are doing test-driven development so we have not
    yet implemented the logpdf function for kmeans before writing the
    tests.

--- tests/test_k_means.py
+++ tests/test_k_means.py
@@ -20,14 +20,15 @@ import sys
 #TODO: create setup.py
 sys.path.insert(0, 'src/k_means')
 
+import numpy as np
+from scipy.stats import multivariate_normal
+
 import k_means
 
 def test_crash_simulate():
-    fm = k_means.KMeans([0,1], [], K=2)
-    fm.simulate(None, [0,1], N=2)
+    km = k_means.KMeans([0,1], [], K=2)
+    km.simulate(None, [0,1], N=2)
 
-def test_crash_logpdf():
-    raise NotImplementedError
 
 def test_crash_transition():
     raise NotImplementedError
@@ -35,23 +36,23 @@ def test_crash_transition():
 # Test whether incorp. and unincorp do the right thing.
 
 def test_incorporate():
-    """Test that after we incorporate for a certain rowid, the dict fm.data has
+    """Test that after we incorporate for a certain rowid, the dict km.data has
     a corresponding entry."""
-    fm = k_means.KMeans([0,1], [], K=2)
-    fm.incorporate(0, {0:2, 1:3})
-    assert fm.data == {0:[2,3]}
+    km = k_means.KMeans([0,1], [], K=2)
+    km.incorporate(0, {0:2, 1:3})
+    assert km.data == {0:[2,3]}
 
 def test_unincorporate():
-    """Test that after we unincorporate for a certain rowid, the dict fm.data
+    """Test that after we unincorporate for a certain rowid, the dict km.data
     has a corresponding entry.
     """
-    fm = k_means.KMeans([0,1], [], K=2)
-    fm.incorporate(0, {0:2, 1:3})
-    fm.unincorporate(0)
+    km = k_means.KMeans([0,1], [], K=2)
+    km.incorporate(0, {0:2, 1:3})
+    km.unincorporate(0)
 
 # Test whether simulate does the right thing.
-def test_compare_to_single_Gaussian():
-    """ Test whether the distubiton of K-means with K>2 is significantly
+def test_compare_to_simulated_samples_to_single_Gaussian():
+    """ Test whether the distubiton of K-means with K=1 is significantly
     different from a single Gaussian.
     """
     raise NotImplementedError
@@ -75,8 +76,79 @@ def test_K_inferred_variances_compared_with_K_known_variances():
     raise NotImplementedError
 
 # Test whether logpdf given generated data does the right thing.
-def test_logdf_compare_k_equals_1_normal():
+def test_logpdf_crash():
+    """Test whether a call to logpdf crashes."""
+    km = k_means.KMeans([0,1], [], K=1)
+    rowid = None
+    query = {0:2.4, 1:1.2}
+    km.logpdf(rowid, query)
+
+def test_logpdf_compare_k_equals_1_normal():
     """Compare the output of logpdf for a k-means CGPM with K=1 with the logdpf
     for a normal disribution."""
-    raise NotImplementedError
+    km = k_means.KMeans([0,1], [], K=1)
+    rowid = None
+    query = {0:2.4, 1:1.2}
+    scipy_mvn = multivariate_normal(mean=[0,0], cov=[[1,0],[0,1]])
+    assert scipy_mvn.pdf([2.4, 12]) == np.exp(km.logpdf(rowid, query))
 
+def test_logpdf_compare_k_equals_2_mixture():
+    """Compare the output of logpdf for a k-means CGPM with K=2 with the logdpf
+    for a normal disribution."""
+    km = k_means.KMeans(
+        [0,1],
+        [],
+        K=2,
+        params={'cluster_centers':[np.array([1,1]), np.array([2,2]),]}
+    )
+    rowid = None
+    query = {0:2.4, 1:1.2}
+    scipy_mvn_k1 = multivariate_normal(mean=[1,1], cov=[[1,0],[0,1]])
+    scipy_mvn_k2 = multivariate_normal(mean=[2,2], cov=[[1,0],[0,1]])
+    # Test assumes that mixing coefficients are both 0.5.
+    expected =\
+        np.log((scipy_mvn_k1.pdf([2.4, 1.2]) + scipy_mvn_k2.pdf([2.4, 1.2]))/2.)
+    actual = km.logpdf(rowid, query)
+    assert expected == actual
+
+def test_logpdf_compare_k_equals_1_normal():
+    """Compare the output of conditioned logpdf for a k-means CGPM with K=1 with
+    the logdpf for a normal disribution.
+
+    Since we restrict the MV to have covariance sigma * I, the joint
+    multivariate normal for col1 and col2 can be written as
+        p(col1, col2) =  p(col1) * p(col1)
+    Thus, co1 and col2 are independent. Ergo, p(col1 | col2)  = p(col1).
+    """
+    km = k_means.KMeans([0,1], [], K=1)
+    rowid = None
+    query = {0:2.4}
+    evidence = {1:1.2}
+    scipy_mvn = multivariate_normal(mean=[0], cov=[1])
+    assert scipy_mvn.pdf([2.4]) ==\
+        np.exp(km.logpdf(rowid, query, evidence=evidence))
+
+def test_logpdf_compare_k_equals_2_mixture():
+    """Compare the output of logpdf for a k-means CGPM with K=2 with the logdpf
+    for a normal disribution."""
+    km = k_means.KMeans(
+        [0,1],
+        [],
+        K=2,
+        params={'cluster_centers':[np.array([1,1]), np.array([2,2]),]}
+    )
+    rowid = None
+    query = {0:2.4}
+    evidence = {1:1.2}
+    # XXX is this correct?
+    scipy_mvn_k1 = multivariate_normal(mean=[1], cov=[1])
+    scipy_mvn_k2 = multivariate_normal(mean=[2], cov=[1])
+    # Test assumes that mixing coefficients are both 0.5.
+    W_1 = 0.5 * scipy_mvn_k1.pdf([1.2])
+    W_2 = 0.5 * scipy_mvn_k2.pdf([1.2])
+    W_denominator = W_1 + W_2
+    W_1 = W_1/W_denominator
+    W_2 = W_2/W_denominator
+    expected = np.log(W_1 * scipy_mvn_k1.pdf([2.4]) + W_2 * scipy_mvn_k2.pdf([2.4]))
+    actual = km.logpdf(rowid, query, evidence=evidence)
+    assert expected == actual
