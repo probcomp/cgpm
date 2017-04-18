015e01fe5b7d8fc57dd19db1dd83340c37220a1d
F Saad <fsaad@mit.edu>
2017-04-18 17:55:43 -0400

    Implement the predictive KL computation.

--- src/crosscat/kl.py
+++ src/crosscat/kl.py
@@ -73,6 +73,44 @@ def compute_pairwise_kl(engine_0, engine_1, num_samples, variables=None):
     return pairwise_kl
 
 
+def compute_predictive_kl(engine_0, engine_1, num_samples, variables=None):
+    """Return KL divergence between states in engine_1 from engine_0."""
+    # Use all variables in the population by default.
+    if variables is None:
+        variables = engine_0.states[0].outputs
+
+    # Generate samples from the states in engine_0.
+    # samples_0[i][k] contains sample k from engine_0.states[i].
+    samples_0 = engine_0.simulate(rowid=None, query=variables, N=num_samples)
+
+    # Resample (no replacement), to simulate the posterior predictive.
+    resamples = engine_0.rng.choice(
+        np.ravel(samples_0), size=num_samples, replace=False)
+
+    # Compute logpdfs of resamples using engine_0.
+    rowids = [-1] * num_samples
+    logpdfs_0 = np.array(engine_0.logpdf_bulk(rowids, resamples))
+    logpdfs_1 = np.array(engine_1.logpdf_bulk(rowids, resamples))
+
+    # Compute predictive logpdf of resamples under engine_0.
+    predictive_logpdfs_0 = [
+        engine_0._likelihood_weighted_integrate(logpdfs_0[:,i], -1)
+        for i in xrange(num_samples)
+    ]
+    # Compute predictive logpdf of resamples under engine_1.
+    predictive_logpdfs_1 = [
+        engine_1._likelihood_weighted_integrate(logpdfs_1[:,i], -1)
+        for i in xrange(num_samples)
+    ]
+
+    # Run sanity check.
+    assert len(predictive_logpdfs_0) == num_samples
+    assert len(predictive_logpdfs_1) == num_samples
+
+    kl = np.mean(np.subtract(predictive_logpdfs_0, predictive_logpdfs_1))
+
+    return kl
+
 def logpdf_bulk_heterogeneous(engine, rowids, queries, evidences=None,
         multiprocess=True):
     """Compute the logpdf in bulk, with different queries per state."""
