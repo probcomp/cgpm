2d2fd6f27ef7535b26581ff356c072187af4748c
Aaron Vinson <avinson.public@gmail.com>
2017-09-07 03:26:26 -0700

    try actually uploading after conda build

--- conda/conda_upload.sh
+++
@@ -1,5 +0,0 @@
-# Only need to change these two variables
-PKG_NAME=cgpm
-
-OS=$TRAVIS_OS_NAME-64
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $CONDA_USER $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
