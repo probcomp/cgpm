e94838ca648aa0c264fb5879003f3e75b175ae18
Feras A Saad <fsaad@mit.edu>
2018-01-24 10:59:43 -0500

    Variable naming.

--- src/venturescript/vscgpm.py
+++ src/venturescript/vscgpm.py
@@ -82,11 +82,11 @@ class VsCGpm(CGpm):
         self.labels = {'observe': dict(), 'predict': dict()}
 
     def incorporate(self, rowid, observation, inputs=None):
-        observation_clean = self._cleanse_observation(rowid, observation)
-        inputs_clean = self._cleanse_inputs(rowid, inputs)
-        for cin, value in inputs_clean.iteritems():
+        observation2 = self._cleanse_observation(rowid, observation)
+        inputs2 = self._cleanse_inputs(rowid, inputs)
+        for cin, value in inputs2.iteritems():
             self._write_input_cell(rowid, cin, value)
-        for cout, value in observation_clean.iteritems():
+        for cout, value in observation2.iteritems():
             self._observe_output_cell(rowid, cout, value)
 
     def unincorporate(self, rowid):
@@ -102,59 +102,58 @@ class VsCGpm(CGpm):
 
     def logpdf(self, rowid, targets, constraints=None, inputs=None,
             accuracy=None):
-        inputs_clean = self._cleanse_inputs(rowid, inputs)
-        targets_clean = self._cleanse_targets(rowid, targets)
-        constraints_clean = self._cleanse_constraints(rowid, targets,
-            constraints)
-        constraints_joint = gu.merged(targets_clean, constraints_clean)
+        inputs2 = self._cleanse_inputs(rowid, inputs)
+        targets2 = self._cleanse_targets(rowid, targets)
+        constraints2 = self._cleanse_constraints(rowid, targets, constraints)
         num_samples = accuracy or 1
         # Write inputs.
-        missing_inputs = [cin for cin in self.inputs if cin not in inputs_clean
+        missing_inputs = [cin for cin in self.inputs if cin not in inputs2
             and not self._is_written_input_cell(rowid, cin)]
         if len(missing_inputs) > 0:
             raise ValueError('Missing logpdf inputs: %s' % (missing_inputs,))
-        for cin, value in inputs_clean.iteritems():
+        for cin, value in inputs2.iteritems():
             self._write_input_cell(rowid, cin, value)
         # Invoke weighted sample on the targets and constraints.
         logps_joint = [
-            self._weighted_forward_sample(rowid, [], constraints_joint)[0]
+            self._weighted_forward_sample(
+                rowid, [], gu.merged(targets2, constraints2))[0]
             for _i in xrange(num_samples)
         ]
         # Invoke weighted sample on the constraints.
         logps_constraints = [
-            self._weighted_forward_sample(rowid, [], constraints_clean)[0]
+            self._weighted_forward_sample(rowid, [], constraints2)[0]
             for _i in xrange(num_samples)
         ]
         # Clear written inputs.
-        for cin in inputs_clean:
+        for cin in inputs2:
             self._clear_input_cell(rowid, cin)
         # Ratio likelihood weighting.
         return gu.logmeanexp(logps_joint) - gu.logmeanexp(logps_constraints)
 
     def simulate(self, rowid, targets, constraints=None, inputs=None, N=None):
-        inputs_clean = self._cleanse_inputs(rowid, inputs)
-        targets_clean = self._cleanse_targets(rowid, targets)
-        constraints_clean = self._cleanse_constraints(
+        inputs2 = self._cleanse_inputs(rowid, inputs)
+        targets2 = self._cleanse_targets(rowid, targets)
+        constraints2 = self._cleanse_constraints(
             rowid, targets, constraints)
         # Write inputs.
-        for cin, value in inputs_clean.iteritems():
+        for cin, value in inputs2.iteritems():
             self._write_input_cell(rowid, cin, value)
         # Observe constraints.
-        for cout, value in constraints_clean.iteritems():
+        for cout, value in constraints2.iteritems():
             self._observe_output_cell(rowid, cout, value)
         # Run local inference in rowid scope, with 15 steps of MH.
         self.ripl.infer('(mh (atom %i) all %i)' % (rowid, 15))
         # Predict targets.
         samples = {cout: self._predict_output_cell(rowid, cout)
-            for cout in targets_clean}
+            for cout in targets2}
         # Forget predicted targets.
-        for cout in targets_clean:
+        for cout in targets2:
             self._unpredict_output_cell(rowid, cout)
         # Forget observed constraints.
-        for cout in constraints_clean:
+        for cout in constraints2:
             self._unobserve_output_cell(rowid, cout)
         # Clear written inputs.
-        for cin in inputs_clean:
+        for cin in inputs2:
             self._clear_input_cell(rowid, cin)
         # Overall samples.
         return samples
